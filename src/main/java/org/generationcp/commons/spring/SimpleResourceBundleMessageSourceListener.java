28125f78ef8baacac38064133794214e3a718bb2
jeff <jeff@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-07-16 10:00:26 +0000

    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPCommons@347 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/commons/spring/SimpleResourceBundleMessageSourceListener.java
+++
@@ -1,8 +0,0 @@
-package org.generationcp.commons.spring;
-
-import java.util.Locale;
-
-public interface SimpleResourceBundleMessageSourceListener {
-    
-    void localeChanged(Locale oldLocale, Locale newLocale);
-}
