3aa1c834d565c498e7e2885688ee9a32e43e1619
kevin <kevin@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-11-08 13:52:50 +0000

    added javadoc headers
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPCommons@1513 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/commons/breedingview/xml/Fieldbook.java
+++ src/main/java/org/generationcp/commons/breedingview/xml/Fieldbook.java
@@ -1,3 +1,16 @@
+/***************************************************************
+ * Copyright (c) 2012, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * @author Kevin L. Manansala
+ * 
+ * This software is licensed for use under the terms of the 
+ * GNU General Public License (http://bit.ly/8Ztv8M) and the 
+ * provisions of Part F of the Generation Challenge Programme 
+ * Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ **************************************************************/
 package org.generationcp.commons.breedingview.xml;
 
 import java.io.Serializable;
@@ -5,17 +18,32 @@ import java.io.Serializable;
 import javax.xml.bind.annotation.XmlAttribute;
 
 
+/**
+ * The Class Fieldbook.
+ */
 public class Fieldbook implements Serializable{
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = -4157597335127419264L;
 
+    /** The file. */
     private String file;
     
+    /**
+     * Gets the file.
+     *
+     * @return the file
+     */
     @XmlAttribute
     public String getFile() {
         return file;
     }
     
+    /**
+     * Sets the file.
+     *
+     * @param file the new file
+     */
     public void setFile(String file) {
         this.file = file;
     }
--- src/test/java/org/generationcp/commons/breedingview/xml/test/TestBreedingViewProjectXml.java
+++ src/test/java/org/generationcp/commons/breedingview/xml/test/TestBreedingViewProjectXml.java
@@ -1,3 +1,16 @@
+/***************************************************************
+ * Copyright (c) 2012, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * @author Kevin L. Manansala
+ * 
+ * This software is licensed for use under the terms of the 
+ * GNU General Public License (http://bit.ly/8Ztv8M) and the 
+ * provisions of Part F of the Generation Challenge Programme 
+ * Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ **************************************************************/
 package org.generationcp.commons.breedingview.xml.test;
 
 import java.util.ArrayList;
