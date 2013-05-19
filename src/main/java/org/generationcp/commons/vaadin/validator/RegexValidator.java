e064a6449e4f960fb44e36592458c487d90de2f0
glenn <glenn@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-19 10:07:31 +0000

    added validation classes
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPCommons@3253 4ebb2e00-0138-4608-bc6e-edf3948d4863

---
+++ src/main/java/org/generationcp/commons/vaadin/validator/RegexValidator.java
@@ -0,0 +1,42 @@
+package org.generationcp.commons.vaadin.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.vaadin.data.validator.AbstractValidator;
+
+/**
+ * A validator for Vaadin input fields.<br>
+ * This validator treats input as {@link String} and performs regular expression
+ * based validation.
+ * 
+ * @author Glenn Marintes
+ */
+public class RegexValidator extends AbstractValidator {
+    private static final long serialVersionUID = 1L;
+    
+    private Pattern pattern;
+    private boolean match;
+
+    public RegexValidator(String errorMessage, String pattern, boolean match) {
+        super(errorMessage);
+        
+        this.pattern = Pattern.compile(pattern);
+        this.match = match;
+    }
+    
+    public RegexValidator(String errorMessage, Pattern pattern, boolean match) {
+        super(errorMessage);
+        
+        this.pattern = pattern;
+        this.match = match;
+    }
+
+    @Override
+    public boolean isValid(Object value) {
+        Matcher matcher = pattern.matcher(value.toString());
+        
+        return match ? matcher.matches() : !matcher.matches();
+    }
+
+}
---
+++ src/main/java/org/generationcp/commons/vaadin/validator/ValidationUtil.java
@@ -0,0 +1,27 @@
+package org.generationcp.commons.vaadin.validator;
+
+import com.vaadin.data.Validator.InvalidValueException;
+
+public class ValidationUtil {
+
+    /**
+     * Get a suitable message for the specified error.
+     * 
+     * @param error
+     * @return
+     */
+    public static String getMessageFor(InvalidValueException error) {
+        String message =  error.getMessage();
+        if (message == null && error.getCause() != null) {
+            message = error.getCause().getMessage();
+        }
+        else if (message == null && error.getCauses() != null) {
+            InvalidValueException[] causes = error.getCauses();
+            if (causes.length > 0) {
+                message = causes[0].getMessage();
+            }
+        }
+        
+        return message == null ? "" : message;
+    }
+}
