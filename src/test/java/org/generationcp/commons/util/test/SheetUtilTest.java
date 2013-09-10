4607b7c12ea8592c13806efc6e254592e096369e
aldrich <aldrichabrogena@gmail.com>
2013-09-10 16:56:11 +0800

    added sheetutil

--- src/test/java/org/generationcp/commons/util/test/SheetUtilTest.java
+++ src/test/java/org/generationcp/commons/util/test/SheetUtilTest.java
@@ -1,42 +1,34 @@
 package org.generationcp.commons.util.test;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.generationcp.commons.util.MySQLUtil;
-import org.generationcp.commons.util.PoiUtil;
+import org.generationcp.commons.util.SheetUtil;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class POIUtilTest {
+public class SheetUtilTest {
     
 	private static String fileName = "C:/Book2.xls";
 	private static Workbook workbook;
-	private static Sheet sheet;
+	
 	
     @BeforeClass
     public static void setup() throws SQLException, ClassNotFoundException, InvalidFormatException, IOException {
     	 FileInputStream inp = new FileInputStream(fileName);
     	 workbook = WorkbookFactory.create(inp);
-		 sheet = workbook.getSheetAt(workbook.getActiveSheetIndex());
+		
     }
 
     @Test
     public void test() throws IOException, InterruptedException {    	
-    	System.out.println( "rowIsEmpty:" + PoiUtil.rowIsEmpty(sheet, 1, 0, 6));
-    	System.out.println( "rowHasEmpty:" + PoiUtil.rowHasEmpty(sheet, 1, 0, 0));
-    	System.out.println( "columnIsEmpty:" + PoiUtil.columnIsEmpty(sheet, 0));
-    	System.out.println( "columnHasEmpty:" + PoiUtil.columnHasEmpty(sheet, 0));
-    	System.out.println( "columnArray:" );
-    	String[] data = PoiUtil.asStringArrayColumn(sheet, 1);
+    	
+    	String[] data = SheetUtil.sheetsToArray(workbook);
     	for(int i=0; i<data.length; i++)
     	{
     		System.out.println( data[i] );
@@ -47,7 +39,7 @@ public class POIUtilTest {
     public static void destory(){
     	
     	 workbook = null;
-		 sheet = null;
+		
     }
     
    
