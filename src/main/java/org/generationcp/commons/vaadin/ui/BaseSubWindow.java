cbef84ca2a41becbcb2e85c9a5765ad74494601f
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-09-12 17:17:20 +0800

    CSS Updates, focus logic moved to BaseSubWindow
    Issue: GCP-9005
    Reviewer: Mae T.

--- src/main/java/org/generationcp/commons/vaadin/ui/BaseSubWindow.java
+++ src/main/java/org/generationcp/commons/vaadin/ui/BaseSubWindow.java
@@ -5,22 +5,29 @@ import com.vaadin.event.Action.Handler;
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.Reindeer;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Configurable;
 
-public class BaseSubWindow extends Window implements Handler {
+@Configurable
+public class BaseSubWindow extends Window implements InitializingBean, Handler {
 	
 	private static final long serialVersionUID = 355237906901597933L;
 	private Action closeAction = new ShortcutAction("ESCAPE", 
 			ShortcutAction.KeyCode.ESCAPE, null);
-	
+
+    private boolean overrideFocus = false;
+    private Window parentWindow;
 
 	public BaseSubWindow() {
-		super();
-		initializeWindow();
-	}
+        super();
+    	this.initializeWindow();
+
+    }
 	
 	public BaseSubWindow(String windowName){
 		super(windowName);
-	}
+        this.initializeWindow();
+    }
 
 	@Override
 	public Action[] getActions(Object target, Object sender) {
@@ -30,6 +37,11 @@ public class BaseSubWindow extends Window implements Handler {
 	@Override
 	public void handleAction(Action action, Object sender, Object target) {
 		if (action == closeAction){
+            // focus to previous window (for nested windows)
+            if (parentWindow != null) {
+                parentWindow.focus();
+            }
+
     		closeWindow();
 		} 
 		
@@ -40,10 +52,31 @@ public class BaseSubWindow extends Window implements Handler {
 	}
 	
 	protected void initializeWindow() {
-		addStyleName(Reindeer.WINDOW_LIGHT);
-		addActionHandler(this);
+		this.addStyleName(Reindeer.WINDOW_LIGHT);
+		this.addActionHandler(this);
+        this.setModal(true);
 	}
 
+    public void setOverrideFocus(boolean val) {
+        overrideFocus = val;
+    }
 
+    public void setParentWindow(Window parentWindow) {
+        this.parentWindow = parentWindow;
+    }
+
+    @Override
+    public void attach() {
+        if (!overrideFocus) {
+            this.focus();
+        }
+
+        super.attach();
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+    }
 }
 
