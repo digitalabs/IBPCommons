ca5366c5cf6adb93e09ed89d953424968bd651fc
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2014-10-01 09:16:24 +0800

    Created new property file and enum class for Common Messages in Vaadin Applications
    GCP-10121
    
    Reviewer: Darla Ani-Hermo

---
+++ src/main/java/org/generationcp/commons/constant/VaadinMessage.java
@@ -0,0 +1,12 @@
+package org.generationcp.commons.constant;
+
+public enum VaadinMessage {
+	
+	//General Labels
+	OK
+	
+	//Date Error Message
+	,ERROR_MONTH_OUT_OF_RANGE
+	,ERROR_DAY_OUT_OF_RANGE
+	,INVALID_YEAR
+}
