fb4309065c9d09720f2745504789718c4164ef03
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-12-12 16:53:50 +0800

    Cleanup / reduce SonarQube violations

--- src/main/java/org/generationcp/commons/security/BMSPreAuthenticatedUsersRolePopulator.java
+++ src/main/java/org/generationcp/commons/security/BMSPreAuthenticatedUsersRolePopulator.java
@@ -1,16 +1,7 @@
 package org.generationcp.commons.security;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.generationcp.commons.context.ContextConstants;
-import org.generationcp.commons.context.ContextInfo;
 import org.generationcp.commons.util.ContextUtil;
-import org.generationcp.commons.util.StringUtil;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
-import org.generationcp.middleware.manager.Operation;
 import org.generationcp.middleware.manager.api.WorkbenchDataManager;
 import org.generationcp.middleware.pojos.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,28 +11,30 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthoritiesContainer;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedGrantedAuthoritiesWebAuthenticationDetails;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BMSPreAuthenticatedUsersRolePopulator implements AuthenticationDetailsSource<HttpServletRequest, GrantedAuthoritiesContainer> {
+public class BMSPreAuthenticatedUsersRolePopulator
+		implements AuthenticationDetailsSource<HttpServletRequest, GrantedAuthoritiesContainer> {
 
 	@Autowired
 	private WorkbenchDataManager workbenchDataManager;
 
 	@Override
 	public GrantedAuthoritiesContainer buildDetails(HttpServletRequest request) {
-
-		ContextInfo contextInfo = ContextUtil.getContextInfoFromRequest(request);
-
 		try {
-			User user = ContextUtil.getCurrentWorkbenchUser(workbenchDataManager,request);
+			User user = ContextUtil.getCurrentWorkbenchUser(workbenchDataManager, request);
 
 			List<GrantedAuthority> role = new ArrayList<>();
 			role.addAll(SecurityUtil.getRolesAsAuthorities(user));
 
-			return new PreAuthenticatedGrantedAuthoritiesWebAuthenticationDetails(request,role);
+			return new PreAuthenticatedGrantedAuthoritiesWebAuthenticationDetails(request, role);
 
 		} catch (MiddlewareQueryException e) {
 
-			throw new AuthenticationServiceException("Data access error while resolving Workbench user roles.", e);
+			throw new AuthenticationServiceException(
+					"Data access error while resolving Workbench user roles.", e);
 		}
 	}
 
