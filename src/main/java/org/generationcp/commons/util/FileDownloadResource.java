5754e6935f30ad7666bd7edc80ac80e59404d9c1
Daniel Jao <daniel@efficio.us.com>
2015-03-27 17:10:23 +0800

    Added code that would handle the download filename and make it utf 8 capable
    BMS-488
    
    Reviewer: Daniel Villafuerte

--- src/main/java/org/generationcp/commons/util/FileDownloadResource.java
+++ src/main/java/org/generationcp/commons/util/FileDownloadResource.java
@@ -16,6 +16,14 @@ package org.generationcp.commons.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vaadin.Application;
 import com.vaadin.terminal.DownloadStream;
@@ -29,10 +37,11 @@ import com.vaadin.terminal.FileResource;
  * 
  */
 public class FileDownloadResource extends FileResource{
-	
+	private static final char[] HEX_CHARS = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
+        'C', 'D', 'E', 'F' };
 	private static final long serialVersionUID = 1L;
 	private String filename = "";
-	
+	private static final Logger LOG = LoggerFactory.getLogger(FileDownloadResource.class);
 	/**
 	 * This resource can be used to stream a file and let the browser detect it as an attachment
 	 * @param sourceFile
@@ -53,14 +62,40 @@ public class FileDownloadResource extends FileResource{
 		this.filename = filename;
 	}
 	
+	public static String getDownloadFileName(String filename, HttpServletRequest request) {
+		String newFilename = filename;
+	      try{
+	    	  if (request != null && (request.getHeader("User-Agent").indexOf("MSIE") != -1 || request.getHeader("User-Agent").indexOf("Trident") != -1)) {
+	              URI uri = new URI(null, null, filename, null);
+	        	  	newFilename = uri.toASCIIString();
+	              return newFilename;
+	            }
+	          byte[] bytes = filename.getBytes("UTF-8");
+	          StringBuilder buff = new StringBuilder(bytes.length << 2);
+	          buff.append("=?UTF-8?Q?");
+	          for (byte b : bytes) {
+	              int unsignedByte = b & 0xFF;
+	              buff.append('=').append(HEX_CHARS[unsignedByte >> 4]).append(HEX_CHARS[unsignedByte & 0xF]);
+	          }
+	          return buff.append("?=").toString();
+	      }catch(UnsupportedEncodingException e){
+	    	  LOG.error(e.getMessage(), e);
+	      } catch (URISyntaxException e) {
+	    	  LOG.error(e.getMessage(), e);
+	    	  
+		}
+	      return newFilename;
+	  }
+	  
 	public DownloadStream getStream() {
 		try {
 			final DownloadStream ds = new DownloadStream(new FileInputStream(
 			getSourceFile()), getMIMEType(), getFilename());
-			ds.setParameter("Content-Disposition", "attachment; filename="+filename);
+			ds.setParameter("Content-Disposition", "attachment; filename="+filename);			
 			ds.setCacheTime(getCacheTime());
 			return ds;
 		} catch (final FileNotFoundException e) {
+			LOG.error(e.getMessage(), e);
 			return null;
 		}
 	}
