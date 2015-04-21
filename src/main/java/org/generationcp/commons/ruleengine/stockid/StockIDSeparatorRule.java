9e3a1aa7fd3dfa5601ccbbd58cbe167c0f47c2df
Daniel Villafuerte <dannyvonline@gmail.com>
2015-04-21 16:23:25 +0800

    Rule engine based implementation for stock id generation  - Context object for storing dependencies and rule execution data  - Rule for breeder identifier / notation number / separator  - Created an interface to abstract the possible implementations on how the system will provide the breeder identifier in the stock id generation process
     BMS-616
    
     Reviewer : Aldrin Batac

---
+++ src/main/java/org/generationcp/commons/ruleengine/stockid/StockIDSeparatorRule.java
@@ -0,0 +1,36 @@
+package org.generationcp.commons.ruleengine.stockid;
+
+import org.apache.commons.lang3.StringUtils;
+import org.generationcp.commons.ruleengine.OrderedRule;
+import org.generationcp.commons.ruleengine.RuleException;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel Villafuerte
+ * Date: 4/16/2015
+ * Time: 3:42 PM
+ */
+@Component
+public class StockIDSeparatorRule extends OrderedRule<StockIDGenerationRuleExecutionContext>{
+
+	public static final String KEY = "SEPARATOR";
+	public static final String DEFAULT_SEPARATOR = "-";
+
+	@Override public Object runRule(StockIDGenerationRuleExecutionContext context)
+			throws RuleException {
+		String separator = context.getSeparator();
+		if (StringUtils.isEmpty(context.getSeparator() ) ) {
+			separator = DEFAULT_SEPARATOR;
+			context.setSeparator(DEFAULT_SEPARATOR);
+		}
+
+		context.getStockIDGenerationBuilder().append(separator);
+
+		return separator;
+	}
+
+	@Override public String getKey() {
+		return KEY;
+	}
+}
