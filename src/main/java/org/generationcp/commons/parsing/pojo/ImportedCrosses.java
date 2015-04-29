150e6aef63514bbf7c6c5381686247969fbc45a3
Daniel Villafuerte <dannyvonline@gmail.com>
2015-04-29 13:46:09 +0800

    Refactored ImportedCrosses to extend ImportedGermplasm
    BMS-668
    
    Reviewer : Jaira Villarino

--- src/main/java/org/generationcp/commons/parsing/pojo/ImportedCrosses.java
+++ src/main/java/org/generationcp/commons/parsing/pojo/ImportedCrosses.java
@@ -12,15 +12,13 @@
 package org.generationcp.commons.parsing.pojo;
 
 import org.generationcp.middleware.pojos.ListDataProject;
-import org.generationcp.middleware.pojos.Name;
 
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * The Class ImportedCrosses.
  */
-public class ImportedCrosses implements Serializable {
+public class ImportedCrosses extends ImportedGermplasm implements Serializable {
 
 	/**
 	 * The Constant serialVersionUID.
@@ -31,11 +29,6 @@ public class ImportedCrosses implements Serializable {
 	private String seedsHarvested;
 	private String crossingDate;
 	private String rawBreedingMethod;
-
-	/**
-	 * The entry id.
-	 */
-	private Integer entryId;
 
 	/**
 	 * The female desig.
@@ -58,69 +51,6 @@ public class ImportedCrosses implements Serializable {
 	private String maleGid;
 
 	/**
-	 * The desig.
-	 */
-	private String desig;
-
-	/**
-	 * The gid.
-	 */
-	private String gid;
-
-	/**
-	 * The cross.
-	 */
-	private String cross;
-
-	/**
-	 * The source.
-	 */
-	private String source;
-
-	/**
-	 * The entry code.
-	 */
-	private String entryCode;
-
-	/**
-	 * The check.
-	 */
-	private String check;
-
-	/**
-	 * The breeding method id.
-	 */
-	private Integer breedingMethodId;
-
-	/**
-	 * Germplasm's GPID1
-	 */
-	private Integer gpid1;
-
-	/**
-	 * Germplasm's GPID2
-	 */
-	private Integer gpid2;
-
-	/**
-	 * Germplasm's Number of Parents
-	 */
-	private Integer gnpgs;
-
-	/**
-	 * List of Names associated with the Germplasm
-	 */
-	private List<Name> names;
-
-	private Integer checkId;
-
-	private String checkName;
-
-	private Integer index;
-
-	private String groupName;
-
-	/**
 	 * Instantiates a new imported germplasm.
 	 */
 	public ImportedCrosses() {
@@ -135,9 +65,7 @@ public class ImportedCrosses implements Serializable {
 	 * @param check   the check
 	 */
 	public ImportedCrosses(Integer entryId, String desig, String check) {
-		this.entryId = entryId;
-		this.desig = desig;
-		this.check = check;
+		super(entryId, desig, check);
 	}
 
 	/**
@@ -153,13 +81,7 @@ public class ImportedCrosses implements Serializable {
 	 */
 	public ImportedCrosses(Integer entryId, String desig, String gid, String cross
 			, String source, String entryCode, String check) {
-		this.entryId = entryId;
-		this.desig = desig;
-		this.gid = gid;
-		this.cross = cross;
-		this.source = source;
-		this.entryCode = entryCode;
-		this.check = check;
+		super(entryId, desig, gid, cross, source, entryCode, check);
 	}
 
 	/**
@@ -202,240 +124,7 @@ public class ImportedCrosses implements Serializable {
 	public ImportedCrosses(Integer entryId, String desig, String gid, String cross
 			, String source, String entryCode, String check, Integer breedingMethodId) {
 
-		this(entryId, desig, gid, cross, source, entryCode, check);
-		this.breedingMethodId = breedingMethodId;
-	}
-
-	public Integer getIndex() {
-		return index;
-	}
-
-	public void setIndex(Integer index) {
-		this.index = index;
-	}
-
-	/**
-	 * Gets the gid.
-	 *
-	 * @return the gid
-	 */
-	public String getGid() {
-		return gid;
-	}
-
-	/**
-	 * Sets the gid.
-	 *
-	 * @param gid the new gid
-	 */
-	public void setGid(String gid) {
-		this.gid = gid;
-	}
-
-	/**
-	 * Gets the cross.
-	 *
-	 * @return the cross
-	 */
-	public String getCross() {
-		return cross;
-	}
-
-	/**
-	 * Sets the cross.
-	 *
-	 * @param cross the new cross
-	 */
-	public void setCross(String cross) {
-		this.cross = cross;
-	}
-
-	/**
-	 * Gets the source.
-	 *
-	 * @return the source
-	 */
-	public String getSource() {
-		return source;
-	}
-
-	/**
-	 * Sets the source.
-	 *
-	 * @param source the new source
-	 */
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	/**
-	 * Gets the entry code.
-	 *
-	 * @return the entry code
-	 */
-	public String getEntryCode() {
-		return entryCode;
-	}
-
-	/**
-	 * Sets the entry code.
-	 *
-	 * @param entryCode the new entry code
-	 */
-	public void setEntryCode(String entryCode) {
-		this.entryCode = entryCode;
-	}
-
-	/**
-	 * Gets the entry id.
-	 *
-	 * @return the entry id
-	 */
-	public Integer getEntryId() {
-		return entryId;
-	}
-
-	/**
-	 * Sets the entry id.
-	 *
-	 * @param entryId the new entry id
-	 */
-	public void setEntryId(Integer entryId) {
-		this.entryId = entryId;
-	}
-
-	/**
-	 * Gets the desig.
-	 *
-	 * @return the desig
-	 */
-	public String getDesig() {
-		return desig;
-	}
-
-	/**
-	 * Sets the desig.
-	 *
-	 * @param desig the new desig
-	 */
-	public void setDesig(String desig) {
-		this.desig = desig;
-	}
-
-	/**
-	 * Gets the check.
-	 *
-	 * @return the check
-	 */
-	public String getCheck() {
-		return check;
-	}
-
-	/**
-	 * Sets the check.
-	 *
-	 * @param check the new check
-	 */
-	public void setCheck(String check) {
-		this.check = check;
-	}
-
-	/**
-	 * @return the breedingMethodId
-	 */
-	public Integer getBreedingMethodId() {
-		return breedingMethodId;
-	}
-
-	/**
-	 * @param breedingMethodId the breedingMethodId to set
-	 */
-	public void setBreedingMethodId(Integer breedingMethodId) {
-		this.breedingMethodId = breedingMethodId;
-	}
-
-	/**
-	 * @return the gpid1
-	 */
-	public Integer getGpid1() {
-		return gpid1;
-	}
-
-	/**
-	 * @param gpid1 the gpid1 to set
-	 */
-	public void setGpid1(Integer gpid1) {
-		this.gpid1 = gpid1;
-	}
-
-	/**
-	 * @return the gpid2
-	 */
-	public Integer getGpid2() {
-		return gpid2;
-	}
-
-	/**
-	 * @param gpid2 the gpid2 to set
-	 */
-	public void setGpid2(Integer gpid2) {
-		this.gpid2 = gpid2;
-	}
-
-	/**
-	 * @return the gnpgs
-	 */
-	public Integer getGnpgs() {
-		return gnpgs;
-	}
-
-	/**
-	 * @param gnpgs the gnpgs to set
-	 */
-	public void setGnpgs(Integer gnpgs) {
-		this.gnpgs = gnpgs;
-	}
-
-	/**
-	 * @return the names
-	 */
-	public List<Name> getNames() {
-		return names;
-	}
-
-	/**
-	 * @param names the names to set
-	 */
-	public void setNames(List<Name> names) {
-		this.names = names;
-	}
-
-	/**
-	 * @return the checkId
-	 */
-	public Integer getCheckId() {
-		return checkId;
-	}
-
-	/**
-	 * @param checkId the checkId to set
-	 */
-	public void setCheckId(Integer checkId) {
-		this.checkId = checkId;
-	}
-
-	/**
-	 * @return the checkName
-	 */
-	public String getCheckName() {
-		return checkName;
-	}
-
-	/**
-	 * @param checkName the checkName to set
-	 */
-	public void setCheckName(String checkName) {
-		this.checkName = checkName;
+		super(entryId, desig, gid, cross, source, entryCode, check, breedingMethodId);
 	}
 
 	/* (non-Javadoc)
@@ -443,42 +132,34 @@ public class ImportedCrosses implements Serializable {
      */
 	@Override
 	public String toString() {
-		return "ImportedCrosses [entryId=" + entryId + ", desig=" + desig
+		return "ImportedCrosses [entryId=" + getEntryId() + ", desig=" + getDesig()
 				+ ", maleDesig=" + maleDesig + ", femaleDesig=" + femaleDesig
 				+ ", maleGid=" + maleGid + ", femaleGid=" + femaleGid
-				+ ", gid=" + gid + ", cross=" + cross + ", source=" + source
-				+ ", entryCode=" + entryCode + ", check=" + check
-				+ ", breedingMethodId=" + breedingMethodId + ", gpid1=" + gpid1
-				+ ", gpid2=" + gpid2 + ", gnpgs=" + gnpgs + ", names=" + names
+				+ ", gid=" + getGid() + ", cross=" + getCross() + ", source=" + getSource()
+				+ ", entryCode=" + getEntryCode() + ", check=" + getCheck()
+				+ ", breedingMethodId=" + getBreedingMethodId() + ", gpid1=" + getGpid1()
+				+ ", gpid2=" + getGpid2() + ", gnpgs=" + getGnpgs() + ", names=" + getNames()
 				+ "]";
 	}
 
 	public ImportedCrosses copy() {
-		ImportedCrosses rec = new ImportedCrosses(this.entryId,
-				this.desig, this.gid, this.cross, this.source, this.entryCode,
-				this.check, this.breedingMethodId);
+		ImportedCrosses rec = new ImportedCrosses(getEntryId(),
+				getDesig(), getGid(), getCross(), getSource(), getEntryCode(),
+				getCheck(), getBreedingMethodId());
 
-		rec.setGpid1(this.gpid1);
-		rec.setGpid2(this.gpid2);
-		rec.setGnpgs(this.gnpgs);
-		rec.setNames(this.names);
-		rec.setCheckId(this.checkId);
-		rec.setCheckName(this.checkName);
-		rec.setIndex(this.index);
+		rec.setGpid1(getGpid1());
+		rec.setGpid2(getGpid2());
+		rec.setGnpgs(getGnpgs());
+		rec.setNames(getNames());
+		rec.setCheckId(getCheckId());
+		rec.setCheckName(getCheckName());
+		rec.setIndex(getIndex());
 		rec.setFemaleDesig(femaleDesig);
 		rec.setMaleDesig(femaleDesig);
 		rec.setFemaleGid(femaleGid);
 		rec.setMaleGid(femaleGid);
 
 		return rec;
-	}
-
-	public String getGroupName() {
-		return groupName;
-	}
-
-	public void setGroupName(String groupName) {
-		this.groupName = groupName;
 	}
 
 	public String getFemaleDesig() {
