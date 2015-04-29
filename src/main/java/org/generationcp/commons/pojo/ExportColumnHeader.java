4342b3b4422dfc55787eadce197b255b1a19918d
Daniel Jao <daniel@efficio.us.com>
2015-04-29 15:40:11 +0800

    Added color styling for the xls cell for the export of stock list
    BMS-695
    
    Reviewer: Crisjaira Villarino

--- src/main/java/org/generationcp/commons/pojo/ExportColumnHeader.java
+++ src/main/java/org/generationcp/commons/pojo/ExportColumnHeader.java
@@ -2,9 +2,13 @@ package org.generationcp.commons.pojo;
 
 public class ExportColumnHeader {
 
+	public static Integer GREEN = 1;
+	public static Integer BLUE = 2;
+	
 	private Integer id;
 	private String name;
 	private boolean isDisplay;
+	private Integer headerColor;
 	
 	public ExportColumnHeader() {
 		super();
@@ -16,7 +20,15 @@ public class ExportColumnHeader {
 		this.name = name;
 		this.isDisplay = isDisplay;
 	}
-
+	
+	public ExportColumnHeader(Integer id, String name, boolean isDisplay, Integer headerColor) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.isDisplay = isDisplay;
+		this.headerColor = headerColor;
+	}
+	
 	public Integer getId() {
 		return this.id;
 	}
@@ -39,6 +51,14 @@ public class ExportColumnHeader {
 
 	public void setDisplay(boolean isDisplay) {
 		this.isDisplay = isDisplay;
+	}
+
+	public Integer getHeaderColor() {
+		return headerColor;
+	}
+
+	public void setHeaderColor(Integer headerColor) {
+		this.headerColor = headerColor;
 	}
 	
 	
