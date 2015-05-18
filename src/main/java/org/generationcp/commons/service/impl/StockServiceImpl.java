17e75de9900647cf9e2a9cb6ab669763208b2829
Daniel Villafuerte <dannyvonline@gmail.com>
2015-05-18 16:09:42 +0800

    Implemented new logic for processing bulk with:  - Separate passes per bulking instruction  - Include logic for processing bulk with of items that don't have reverse duplicate information
    BMS-654
    
    Reviewer : Jai Villarino

--- src/main/java/org/generationcp/commons/service/impl/StockServiceImpl.java
+++ src/main/java/org/generationcp/commons/service/impl/StockServiceImpl.java
@@ -11,10 +11,7 @@ import org.generationcp.middleware.pojos.ListDataProject;
 import org.generationcp.middleware.service.api.InventoryService;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,93 +21,146 @@ import java.util.Map;
  */
 public class StockServiceImpl implements StockService {
 
-	@Resource
-	private RulesService rulesService;
+    @Resource
+    private RulesService rulesService;
 
-	@Resource
-	private RuleFactory ruleFactory;
+    @Resource
+    private RuleFactory ruleFactory;
 
-	@Resource
-	private InventoryService inventoryService;
+    @Resource
+    private InventoryService inventoryService;
 
-	@Override public void assignStockIDs(List<InventoryDetails> details)
-			throws MiddlewareException {
+    @Override
+    public void assignStockIDs(List<InventoryDetails> details)
+            throws MiddlewareException {
 
-		this.assignStockIDs(details, null, null);
-	}
+        this.assignStockIDs(details, null, null);
+    }
 
-	@Override public void assignStockIDs(List<InventoryDetails> details, String breederIdentifier,
-			String separator)
-			throws MiddlewareException {
-		String stockIDPrefix = calculateNextStockIDPrefix(breederIdentifier, separator);
+    @Override
+    public void assignStockIDs(List<InventoryDetails> details, String breederIdentifier,
+                               String separator)
+            throws MiddlewareException {
+        String stockIDPrefix = calculateNextStockIDPrefix(breederIdentifier, separator);
 
-		for (InventoryDetails detail : details) {
-			detail.setInventoryID(stockIDPrefix + detail.getEntryId());
-		}
-	}
+        for (InventoryDetails detail : details) {
+            detail.setInventoryID(stockIDPrefix + detail.getEntryId());
+        }
+    }
 
-	/**
-	 *
-	 * Returns the stock ID prefix (consisting of breeder identifier plus current notation number) and the configured separator
-	 * e.g., 'DV1-'
-	 * @param breederIdentifier
-	 * @param separator
-	 * @return
-	 * @throws MiddlewareException
-	 */
-	@Override
-	public String calculateNextStockIDPrefix(String breederIdentifier, String separator) throws
-			MiddlewareException {
-		List<String> sequenceList = Arrays
-				.asList(ruleFactory.getRuleSequenceForNamespace("stockid"));
-		StockIDGenerationRuleExecutionContext context = new StockIDGenerationRuleExecutionContext(
-				sequenceList, inventoryService);
-		context.setBreederIdentifier(breederIdentifier);
-		context.setSeparator(separator);
+    /**
+     * Returns the stock ID prefix (consisting of breeder identifier plus current notation number) and the configured separator
+     * e.g., 'DV1-'
+     *
+     * @param breederIdentifier
+     * @param separator
+     * @return
+     * @throws MiddlewareException
+     */
+    @Override
+    public String calculateNextStockIDPrefix(String breederIdentifier, String separator) throws
+            MiddlewareException {
+        List<String> sequenceList = Arrays
+                .asList(ruleFactory.getRuleSequenceForNamespace("stockid"));
+        StockIDGenerationRuleExecutionContext context = new StockIDGenerationRuleExecutionContext(
+                sequenceList, inventoryService);
+        context.setBreederIdentifier(breederIdentifier);
+        context.setSeparator(separator);
 
-		try {
-			rulesService.runRules(context);
-			return (String) context.getRuleExecutionOutput();
+        try {
+            rulesService.runRules(context);
+            return (String) context.getRuleExecutionOutput();
 
-		} catch (RuleException e) {
-			throw new MiddlewareException(e.getMessage(), e);
-		}
-	}
+        } catch (RuleException e) {
+            throw new MiddlewareException(e.getMessage(), e);
+        }
+    }
 
-	@Override
-	public void processBulkSettings(Collection<ListDataProject> dataProjectList, Map<Integer, InventoryDetails> inventoryDetailsMap,
-									boolean addPedigreeDuplicate, boolean addPlotReciprocal, boolean addPedigreeReciprocal) {
-		for (ListDataProject listDataProject : dataProjectList) {
-			if (addPedigreeDuplicate) {
-				processBulkWith(listDataProject.getEntryId(), listDataProject.parsePedigreeDupeInformation(), inventoryDetailsMap);
-			}
+    @Override
+    public void processBulkSettings(Collection<ListDataProject> dataProjectList, Map<Integer, InventoryDetails> inventoryDetailsMap,
+                                    boolean addPedigreeDuplicate, boolean addPlotReciprocal, boolean addPedigreeReciprocal) {
 
-			if (addPlotReciprocal) {
-				processBulkWith(listDataProject.getEntryId(), listDataProject.parsePlotReciprocalInformation(), inventoryDetailsMap);
-			}
+        Map<Integer, ListDataProject> projectMap = new HashMap<>();
 
-			if (addPedigreeReciprocal) {
-				processBulkWith(listDataProject.getEntryId(), listDataProject.parsePedigreeReciprocalInformation(), inventoryDetailsMap);
-			}
-		}
-	}
+        for (ListDataProject project : dataProjectList) {
+            projectMap.put(project.getEntryId(), project);
+        }
 
-	protected void processBulkWith(Integer targetEntryID, List<Integer> entryIds, Map<Integer, InventoryDetails> detailMap) {
-		InventoryDetails target = detailMap.get(targetEntryID);
+        if (addPedigreeDuplicate) {
+            processBulkWith(projectMap, new RetrieveParseIDStrategy() {
+                @Override
+                public List<Integer> retrieveParsedIDsForProcessing(ListDataProject project) {
+                    return project.parsePedigreeDupeInformation();
+                }
+            }, inventoryDetailsMap);
+        }
 
-		if (entryIds.size() == 0) {
-			return;
-		}
+        if (addPlotReciprocal) {
+            processBulkWith(projectMap, new RetrieveParseIDStrategy() {
+                @Override
+                public List<Integer> retrieveParsedIDsForProcessing(ListDataProject project) {
+                    return project.parsePlotReciprocalInformation();
+                }
+            }, inventoryDetailsMap);
+        }
 
-		target.setBulkCompl("Y");
+        if (addPedigreeReciprocal) {
+            processBulkWith(projectMap, new RetrieveParseIDStrategy() {
+                @Override
+                public List<Integer> retrieveParsedIDsForProcessing(ListDataProject project) {
+                    return project.parsePedigreeReciprocalInformation();
+                }
+            }, inventoryDetailsMap);
+        }
 
-		for (Integer entryId : entryIds) {
-			InventoryDetails bulkDetail = detailMap.get(entryId);
-			if (bulkDetail == null) {
-				continue;
-			}
+    }
 
-			target.addBulkWith(bulkDetail.getInventoryID());
-		}
-	}
+    protected void processBulkWith(Map<Integer, ListDataProject> projectMap, RetrieveParseIDStrategy strategy,
+                                   Map<Integer, InventoryDetails> detailMap) {
+        List<Integer> processedEntries = new ArrayList<>();
+
+        for (ListDataProject project : projectMap.values()) {
+            List<Integer> parsedIDs = strategy.retrieveParsedIDsForProcessing(project);
+
+            if (parsedIDs.isEmpty()) {
+                continue;
+            }
+
+            processEntry(project.getEntryId(), projectMap, strategy, detailMap, processedEntries);
+        }
+
+    }
+
+    protected void processEntry(Integer targetEntryID, Map<Integer, ListDataProject> projectMap, RetrieveParseIDStrategy strategy,
+                                Map<Integer, InventoryDetails> detailMap, List<Integer> processedEntries) {
+
+        if( processedEntries.contains(targetEntryID)) {
+            return;
+        }
+
+        List<Integer> parsedIDs = strategy.retrieveParsedIDsForProcessing(projectMap.get(targetEntryID));
+
+        InventoryDetails target = detailMap.get(targetEntryID);
+
+        target.setBulkCompl("Y");
+        processedEntries.add(targetEntryID);
+        for (Integer parsedId : parsedIDs) {
+
+            InventoryDetails bulkDetail = detailMap.get(parsedId);
+
+            if (bulkDetail == null) {
+                continue;
+            }
+
+            target.addBulkWith(bulkDetail.getInventoryID());
+            bulkDetail.addBulkWith(target.getInventoryID());
+            processEntry(parsedId, projectMap, strategy, detailMap, processedEntries);
+        }
+
+
+    }
+
+    interface RetrieveParseIDStrategy {
+        List<Integer> retrieveParsedIDsForProcessing(ListDataProject project);
+    }
 }
