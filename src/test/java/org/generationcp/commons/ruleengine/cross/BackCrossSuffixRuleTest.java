8670441ea1b15374dca4dcd5e09c60bd2b0cb75c
Daniel Villafuerte <dannyvonline@gmail.com>
2015-09-16 19:17:15 +0800

    Replaced anyInt calls to use the specific parameter Reduced scope visibility of constants Applied formatting
    BMS-1547
    Reviewer : Cyrus Venn Casada

--- src/test/java/org/generationcp/commons/ruleengine/cross/BackCrossSuffixRuleTest.java
+++ src/test/java/org/generationcp/commons/ruleengine/cross/BackCrossSuffixRuleTest.java
@@ -12,70 +12,67 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-
 public class BackCrossSuffixRuleTest {
 
-	public static final String TEST_BASE_CROSS_NAME = "Cross";
-    public static final int MALE_GID = 100;
-    public static final int FEMALE_GID = 101;
+	private static final String TEST_BASE_CROSS_NAME = "Cross";
+	private static final int MALE_GID = 100;
+	private static final int FEMALE_GID = 101;
 
 	private BackCrossSuffixRule unitUnderTest;
-    private PedigreeDataManager pedigreeDataManager;
-    private CrossSetting crossSetting;
-    private GermplasmDataManager germplasmDataManager;
+	private PedigreeDataManager pedigreeDataManager;
+	private CrossSetting crossSetting;
+	private GermplasmDataManager germplasmDataManager;
 	private CrossingRuleExecutionContext ruleContext;
-
-
 
 	@Before
 	public void setUp() throws Exception {
 		unitUnderTest = new BackCrossSuffixRule();
 
 		pedigreeDataManager = Mockito.mock(PedigreeDataManager.class);
-        crossSetting = Mockito.mock(CrossSetting.class);
-        germplasmDataManager = Mockito.mock(GermplasmDataManager.class);
+		crossSetting = Mockito.mock(CrossSetting.class);
+		germplasmDataManager = Mockito.mock(GermplasmDataManager.class);
 		ruleContext = new CrossingRuleExecutionContext(null);
 		ruleContext.setPedigreeDataManager(pedigreeDataManager);
 		ruleContext.setCurrentCrossName(TEST_BASE_CROSS_NAME);
-        ruleContext.setCrossSetting(crossSetting);
-        ruleContext.setGermplasmDataManager(germplasmDataManager);
-        ruleContext.setMaleGid(MALE_GID);
-        ruleContext.setFemaleGid(FEMALE_GID);
+		ruleContext.setCrossSetting(crossSetting);
+		ruleContext.setGermplasmDataManager(germplasmDataManager);
+		ruleContext.setMaleGid(MALE_GID);
+		ruleContext.setFemaleGid(FEMALE_GID);
 	}
 
 	@Test
 	public void testRunRuleMaleRecurrent() throws RuleException, MiddlewareQueryException {
-		Mockito.when(pedigreeDataManager.calculateRecurrentParent(Mockito.anyInt(), Mockito.anyInt())).thenReturn(
-                PedigreeDataManager.MALE_RECURRENT);
+		Mockito.when(pedigreeDataManager.calculateRecurrentParent(MALE_GID, FEMALE_GID)).thenReturn(PedigreeDataManager.MALE_RECURRENT);
 
 		String output = (String) unitUnderTest.runRule(ruleContext);
-		Assert.assertEquals("Unable to add proper suffix for items with recurrent male parent", TEST_BASE_CROSS_NAME + BackCrossSuffixRule.MALE_RECURRENT_SUFFIX, output);
+		Assert.assertEquals("Unable to add proper suffix for items with recurrent male parent", TEST_BASE_CROSS_NAME
+				+ BackCrossSuffixRule.MALE_RECURRENT_SUFFIX, output);
 	}
 
 	@Test
-	public void testRunRuleFemaleRecurrent() throws RuleException, MiddlewareQueryException  {
-		Mockito.when(pedigreeDataManager.calculateRecurrentParent(Mockito.anyInt(), Mockito.anyInt())).thenReturn(
-				PedigreeDataManager.FEMALE_RECURRENT);
+	public void testRunRuleFemaleRecurrent() throws RuleException, MiddlewareQueryException {
+		Mockito.when(pedigreeDataManager.calculateRecurrentParent(MALE_GID, FEMALE_GID)).thenReturn(PedigreeDataManager.FEMALE_RECURRENT);
 
 		String output = (String) unitUnderTest.runRule(ruleContext);
-		Assert.assertEquals("Unable to add proper suffix for items with recurrent female parent", TEST_BASE_CROSS_NAME + BackCrossSuffixRule.FEMALE_RECURRENT_SUFFIX, output);
+		Assert.assertEquals("Unable to add proper suffix for items with recurrent female parent", TEST_BASE_CROSS_NAME
+				+ BackCrossSuffixRule.FEMALE_RECURRENT_SUFFIX, output);
 	}
 
 	@Test
-	public void testRunRuleNonRecurrent() throws RuleException, MiddlewareQueryException  {
-		Mockito.when(pedigreeDataManager.calculateRecurrentParent(Mockito.anyInt(), Mockito.anyInt())).thenReturn(PedigreeDataManager.NONE);
+	public void testRunRuleNonRecurrent() throws RuleException, MiddlewareQueryException {
+		Mockito.when(pedigreeDataManager.calculateRecurrentParent(MALE_GID, FEMALE_GID)).thenReturn(PedigreeDataManager.NONE);
 
 		String output = (String) unitUnderTest.runRule(ruleContext);
 		Assert.assertEquals("Wrong output for items with no recurrent parent", TEST_BASE_CROSS_NAME, output);
 	}
 
-    @Test
-    public void testGetKey() {
-        Assert.assertEquals(BackCrossSuffixRule.KEY, unitUnderTest.getKey());
-    }
+	@Test
+	public void testGetKey() {
+		Assert.assertEquals(BackCrossSuffixRule.KEY, unitUnderTest.getKey());
+	}
 
-    @Test
-    public void testGetProcessCode() {
-        Assert.assertEquals(BackCrossSuffixRule.PROCESS_CODE, unitUnderTest.getProcessCode());
-    }
+	@Test
+	public void testGetProcessCode() {
+		Assert.assertEquals(BackCrossSuffixRule.PROCESS_CODE, unitUnderTest.getProcessCode());
+	}
 }
