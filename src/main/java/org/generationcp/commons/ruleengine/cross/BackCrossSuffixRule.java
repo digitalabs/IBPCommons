24ac402857de8bd41a5788f6db357d28c65217d8
Daniel Villafuerte <dannyvonline@gmail.com>
2015-09-16 19:19:48 +0800

    Reduced visibility of constant Applied formatting
    BMS-1547
    Reviewer : Cyrus Venn Casada

--- src/main/java/org/generationcp/commons/ruleengine/cross/BackCrossSuffixRule.java
+++ src/main/java/org/generationcp/commons/ruleengine/cross/BackCrossSuffixRule.java
@@ -1,12 +1,10 @@
+
 package org.generationcp.commons.ruleengine.cross;
 
-import org.generationcp.commons.ruleengine.OrderedRule;
 import org.generationcp.commons.ruleengine.ProcessCodeOrderedRule;
 import org.generationcp.commons.ruleengine.RuleException;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.manager.api.PedigreeDataManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,41 +13,39 @@ import org.springframework.stereotype.Component;
 @Component
 public class BackCrossSuffixRule extends ProcessCodeOrderedRule<CrossingRuleExecutionContext> {
 
-    public static final String KEY = "BACKCROSSSUFFIX";
-    public static final String MALE_RECURRENT_SUFFIX = "BM";
-    public static final String FEMALE_RECURRENT_SUFFIX = "BF";
-    public static final String PROCESS_CODE = "[BC]";
+	static final String KEY = "BACKCROSSSUFFIX";
+	static final String MALE_RECURRENT_SUFFIX = "BM";
+	static final String FEMALE_RECURRENT_SUFFIX = "BF";
+	static final String PROCESS_CODE = "[BC]";
 
-    @Override
-    public String getKey() {
-        return KEY;
-    }
+	@Override
+	public String getKey() {
+		return KEY;
+	}
 
-    @Override
-    public Object runRule(CrossingRuleExecutionContext context) throws RuleException {
-        try {
-            int computation = context.getPedigreeDataManager().calculateRecurrentParent(context.getMaleGid(),
-                    context.getFemaleGid());
+	@Override
+	public Object runRule(CrossingRuleExecutionContext context) throws RuleException {
+		try {
+			int computation = context.getPedigreeDataManager().calculateRecurrentParent(context.getMaleGid(), context.getFemaleGid());
 
-            String output = context.getCurrentCrossName() == null? "" : context.getCurrentCrossName();
-            if (PedigreeDataManager.FEMALE_RECURRENT == computation) {
-                output += FEMALE_RECURRENT_SUFFIX;
-            } else if (PedigreeDataManager.MALE_RECURRENT == computation) {
-                output += MALE_RECURRENT_SUFFIX;
-            }
+			String output = context.getCurrentCrossName() == null ? "" : context.getCurrentCrossName();
+			if (PedigreeDataManager.FEMALE_RECURRENT == computation) {
+				output += FEMALE_RECURRENT_SUFFIX;
+			} else if (PedigreeDataManager.MALE_RECURRENT == computation) {
+				output += MALE_RECURRENT_SUFFIX;
+			}
 
-            context.setCurrentCrossName(output);
+			context.setCurrentCrossName(output);
 
-            return output;
-        } catch (MiddlewareQueryException e) {
-            throw  new RuleException(e.getMessage(), e);
-        }
+			return output;
+		} catch (MiddlewareQueryException e) {
+			throw new RuleException(e.getMessage(), e);
+		}
 
+	}
 
-    }
-
-    @Override
-    public String getProcessCode() {
-        return PROCESS_CODE;
-    }
+	@Override
+	public String getProcessCode() {
+		return PROCESS_CODE;
+	}
 }
