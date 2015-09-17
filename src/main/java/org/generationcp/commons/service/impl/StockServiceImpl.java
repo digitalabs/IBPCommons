fa5468a2c09f9f681fa44ac529a315e58644c7d8
Mae Isabelle Turiana <maeisabelle@gmail.com>
2015-09-17 10:52:49 +0800

    Provided a more meaningful Java documentation for StockServiceImpl on what is used for
    BMS-1658
    
    Reviewer: Crisjaira Villarino

--- src/main/java/org/generationcp/commons/service/impl/StockServiceImpl.java
+++ src/main/java/org/generationcp/commons/service/impl/StockServiceImpl.java
@@ -34,7 +34,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Created by IntelliJ IDEA. User: Daniel Villafuerte Date: 4/16/2015 Time: 2:51 PM
+ * This class is used for providing stock-related functionalities such as assigning stock IDs, processing bulk settings and bulking
+ * duplicates and reciprocals
  */
 @Transactional
 public class StockServiceImpl implements StockService {
