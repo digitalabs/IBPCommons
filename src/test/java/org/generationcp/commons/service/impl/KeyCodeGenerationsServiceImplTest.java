f0f38d0a764b47a051086e353d257e2f3019624b
Naymesh <naymesh@leafnode.io>
2016-03-29 11:05:02 +1300

    More unit test cases for optional and required key components scenario.
    Issue: BMS-2579
    Reviewer: MatthewB

--- src/test/java/org/generationcp/commons/service/impl/KeyCodeGenerationsServiceImplTest.java
+++ src/test/java/org/generationcp/commons/service/impl/KeyCodeGenerationsServiceImplTest.java
@@ -14,65 +14,132 @@ import junit.framework.Assert;
 
 public class KeyCodeGenerationsServiceImplTest {
 
+	// Template with one optional component ([SELECTION_NUMBER])
+	final KeyTemplateProvider oneOptionalComponentTemplateProvider = new KeyTemplateProvider() {
+
+		@Override
+		public String getKeyTemplate() {
+			return "[LOCATION]-[PLOTNO][SELECTION_NUMBER]";
+		}
+	};
+
+	// Template with all required components
+	final KeyTemplateProvider allRequiredComponentTemplateProvider = new KeyTemplateProvider() {
+
+		@Override
+		public String getKeyTemplate() {
+			return "[LOCATION]-[PLOTNO]-[SEASON]";
+		}
+	};
+
+	final KeyComponentValueResolver locationValueResolver = new KeyComponentValueResolver() {
+
+		@Override
+		public String resolve() {
+			return "INDIA";
+		}
+
+		@Override
+		public boolean isOptional() {
+			return false;
+		}
+	};
+
+	final KeyComponentValueResolver plotNumberResolver = new KeyComponentValueResolver() {
+
+		@Override
+		public String resolve() {
+			return "123";
+		}
+
+		@Override
+		public boolean isOptional() {
+			return false;
+		}
+	};
+
+	final KeyComponentValueResolver seasonResolver = new KeyComponentValueResolver() {
+
+		@Override
+		public String resolve() {
+			return "DrySeason";
+		}
+
+		@Override
+		public boolean isOptional() {
+			return false;
+		}
+	};
+
+	final KeyComponentValueResolver optionalSelectionNumberWithValueResolver = new KeyComponentValueResolver() {
+
+		@Override
+		public String resolve() {
+			return "Plant1";
+		}
+
+		@Override
+		public boolean isOptional() {
+			return true;
+		}
+	};
+
+	final KeyComponentValueResolver optionalSelectionNumberWithoutValueResolver = new KeyComponentValueResolver() {
+
+		@Override
+		public String resolve() {
+			return null;
+		}
+
+		@Override
+		public boolean isOptional() {
+			return true;
+		}
+	};
+
 	@Test
-	public void testGenerateKey() {
+	public void testGenerateKeyAllRequiredComponents() {
 
 		KeyCodeGenerationService service = new KeyCodeGenerationServiceImpl();
 
-		KeyTemplateProvider inMemoryKeyTemplateProvider = new KeyTemplateProvider() {
+		Map<KeyComponent, KeyComponentValueResolver> keyComponentValueResolvers = new HashMap<>();
+		keyComponentValueResolvers.put(KeyComponent.LOCATION, this.locationValueResolver);
+		keyComponentValueResolvers.put(KeyComponent.PLOTNO, this.plotNumberResolver);
+		keyComponentValueResolvers.put(KeyComponent.SEASON, this.seasonResolver);
 
-			@Override
-			public String getKeyTemplate() {
-				return "[LOCATION]-[PLOTNO][SELECTION_NUMBER]";
-			}
-		};
+		String keyCode = service.generateKey(this.allRequiredComponentTemplateProvider, keyComponentValueResolvers);
 
-		final KeyComponentValueResolver locationValueResolver = new KeyComponentValueResolver() {
+		Assert.assertEquals("INDIA-123-DrySeason", keyCode);
+	}
 
-			@Override
-			public String resolve() {
-				return "INDIA";
-			}
+	@Test
+	public void testGenerateKeyOneOptionalComponentWithValue() {
 
-			@Override
-			public boolean isOptional() {
-				return false;
-			}
-		};
-
-		final KeyComponentValueResolver plotNumberResolver = new KeyComponentValueResolver() {
-
-			@Override
-			public String resolve() {
-				return "123";
-			}
-
-			@Override
-			public boolean isOptional() {
-				return false;
-			}
-		};
-
-		final KeyComponentValueResolver selectionNumberResolver = new KeyComponentValueResolver() {
-
-			@Override
-			public String resolve() {
-				return "456";
-			}
-
-			@Override
-			public boolean isOptional() {
-				return true;
-			}
-		};
+		KeyCodeGenerationService service = new KeyCodeGenerationServiceImpl();
 
 		Map<KeyComponent, KeyComponentValueResolver> keyComponentValueResolvers = new HashMap<>();
-		keyComponentValueResolvers.put(KeyComponent.LOCATION, locationValueResolver);
-		keyComponentValueResolvers.put(KeyComponent.PLOTNO, plotNumberResolver);
-		keyComponentValueResolvers.put(KeyComponent.SELECTION_NUMBER, selectionNumberResolver);
+		keyComponentValueResolvers.put(KeyComponent.LOCATION, this.locationValueResolver);
+		keyComponentValueResolvers.put(KeyComponent.PLOTNO, this.plotNumberResolver);
+		keyComponentValueResolvers.put(KeyComponent.SELECTION_NUMBER, this.optionalSelectionNumberWithValueResolver);
 
-		String businessKey = service.generateKey(inMemoryKeyTemplateProvider, keyComponentValueResolvers);
+		String keyCode = service.generateKey(this.oneOptionalComponentTemplateProvider, keyComponentValueResolvers);
 
-		Assert.assertEquals("INDIA-123-456", businessKey);
+		Assert.assertEquals("INDIA-123-Plant1", keyCode);
 	}
+
+	@Test
+	public void testGenerateKeyOneOptionalComponentWithoutValue() {
+
+		KeyCodeGenerationService service = new KeyCodeGenerationServiceImpl();
+
+		Map<KeyComponent, KeyComponentValueResolver> keyComponentValueResolvers = new HashMap<>();
+		keyComponentValueResolvers.put(KeyComponent.LOCATION, this.locationValueResolver);
+		keyComponentValueResolvers.put(KeyComponent.PLOTNO, this.plotNumberResolver);
+		keyComponentValueResolvers.put(KeyComponent.SELECTION_NUMBER, this.optionalSelectionNumberWithoutValueResolver);
+
+		String keyCode = service.generateKey(this.oneOptionalComponentTemplateProvider, keyComponentValueResolvers);
+
+		Assert.assertEquals("INDIA-123", keyCode);
+	}
+
 }
