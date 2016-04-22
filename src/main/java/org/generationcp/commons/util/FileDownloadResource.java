c3beff5a89824a580d065205b66c67f6e10bdb56
Cyrus Venn Casada <cvpcasada@outlook.com>
2016-04-22 11:57:21 +0800

    Updated FileDownloadResource to use the updated content-disposition format
    BMS-2522

--- src/main/java/org/generationcp/commons/util/FileDownloadResource.java
+++ src/main/java/org/generationcp/commons/util/FileDownloadResource.java
@@ -1,90 +1,62 @@
 /***************************************************************
  * F * Copyright (c) 2012, All Rights Reserved.
- * 
+ *
  * Generation Challenge Programme (GCP)
- * 
+ *
  * @author Kevin L. Manansala
- * 
- *         This software is licensed for use under the terms of the GNU General Public License (http://bit.ly/8Ztv8M) and the provisions of
- *         Part F of the Generation Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
- * 
+ *
+ * This software is licensed for use under the terms of the GNU General Public License (http://bit.ly/8Ztv8M) and the provisions of
+ * Part F of the Generation Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
  **************************************************************/
 
 package org.generationcp.commons.util;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.Application;
-import com.vaadin.service.FileTypeResolver;
 import com.vaadin.terminal.DownloadStream;
 import com.vaadin.terminal.FileResource;
 
 /**
  * This class has utility methods used by other classes.
- * 
+ *
  * @author Dennis Billano
- * 
+ *
  */
 public class FileDownloadResource extends FileResource {
 
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = LoggerFactory.getLogger(FileDownloadResource.class);
-	private String filename = "";
-	private String userAgent = "";
 
-	/**
-	 * This resource can be used to stream a file and let the browser detect it as an attachment
-	 * 
-	 * @param sourceFile
-	 * @param application
-	 */
-	public FileDownloadResource(File sourceFile, Application application, String userAgent) {
+	private String downloadFileName;
+
+	public FileDownloadResource(File sourceFile, Application application) {
 		super(sourceFile, application);
-		this.setFilename(super.getFilename());
-		this.userAgent = userAgent;
+	}
+
+	public FileDownloadResource(File sourceFile, String downloadFileName, Application application) {
+		super(sourceFile, application);
+		this.downloadFileName = FileUtils.sanitizeFileName(downloadFileName);
 	}
 
 	@Override
 	public String getFilename() {
-		return this.filename;
-	}
-
-	/**
-	 * Use this method to set the downloaded filename, if not used, the downloaded filename will be the same as the filename of the
-	 * sourceFile
-	 * 
-	 * @param filename
-	 */
-	public void setFilename(String filename) {
-		this.filename = filename;
+		return downloadFileName == null ? super.getFilename() : downloadFileName;
 	}
 
 	@Override
 	public DownloadStream getStream() {
-		try {
-			final DownloadStream ds =
-					new DownloadStream(new FileInputStream(this.getSourceFile()), FileTypeResolver.getMIMEType(this.filename),
-							this.getFilename());
+		final DownloadStream ds = super.getStream();
+		final String mimeType = FileUtils.detectMimeType(this.getFilename());
 
-			if (this.userAgent.indexOf("MSIE") != -1 || this.userAgent.indexOf("Trident") != -1 || this.userAgent.indexOf("Safari") != -1) {
-				// Internet Explorer has problems reading the Content-disposition header if it contains "filename*"
-				ds.setParameter("Content-Disposition", "attachment; filename=\"" + this.filename + "\";");
-			} else {
-				// Those user agents that do not support the RFC 5987 encoding ignore "filename*" when it occurs after "filename".
-				ds.setParameter("Content-Disposition", "attachment; filename=\"" + this.filename + "\"; filename*=\"UTF-8''"
-						+ this.filename + "\";");
-			}
+		ds.setParameter("Content-Type", String.format("%s;charset=utf-8", mimeType));
+		ds.setParameter("Content-Disposition", String.format("attachment; filename=\"%s\"; filename*=utf-8''%s", this.getFilename(),
+				FileUtils.encodeFilenameForDownload(this.getFilename())));
 
-			ds.setCacheTime(this.getCacheTime());
-			return ds;
-		} catch (final FileNotFoundException e) {
-			FileDownloadResource.LOG.error(e.getMessage(), e);
-			return null;
-		}
+		return ds;
 	}
+
 }
