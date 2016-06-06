20d0e7e2fa767efe0e6efd1b61b2992368833d8c
Aldrin Batac <aldrin@leafnode.io>
2016-06-06 13:07:04 +0800

    Added documentation and new sanitize method without trimming spaces.
    BMS-2835

--- src/main/java/org/generationcp/commons/util/BreedingViewUtil.java
+++ src/main/java/org/generationcp/commons/util/BreedingViewUtil.java
@@ -7,10 +7,34 @@ public class BreedingViewUtil {
 
 	public static final String REGEX_VALID_BREEDING_VIEW_CHARACTERS = "[^a-zA-Z0-9-_%']+";
 
+	/**
+	 * Trim the spaces and replaces all Breeding View's invalid characters in a variable name with an underscore.
+	 * Only alphanumeric (a-z A-Z), dash (-), underscore (_) and percentage (%) characters are allowed in Breeding View.
+	 *
+	 * @param name
+	 * @return
+	 */
 	public static String trimAndSanitizeName(String name) {
 
 		if (name != null) {
-			return name.trim().replaceAll(REGEX_VALID_BREEDING_VIEW_CHARACTERS, "_");
+			return sanitizeName(name.trim());
+		} else {
+			return name;
+		}
+
+	}
+
+	/**
+	 * Replaces all Breeding View's invalid characters in a variable name with an underscore.
+	 * Only alphanumeric (a-z A-Z), dash (-), underscore (_) and percentage (%) characters are allowed in Breeding View.
+	 *
+	 * @param name
+	 * @return
+	 */
+	public static String sanitizeName(String name) {
+
+		if (name != null) {
+			return name.replaceAll(REGEX_VALID_BREEDING_VIEW_CHARACTERS, "_");
 		} else {
 			return name;
 		}
