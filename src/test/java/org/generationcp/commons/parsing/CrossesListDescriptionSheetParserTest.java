f59c2492262acf7f77bc102accf138c4540cfcf3
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-08-12 01:37:33 +0800

    Applied leafnode clean up
    BMS-3181

--- src/test/java/org/generationcp/commons/parsing/CrossesListDescriptionSheetParserTest.java
+++ src/test/java/org/generationcp/commons/parsing/CrossesListDescriptionSheetParserTest.java
@@ -1,3 +1,4 @@
+
 package org.generationcp.commons.parsing;
 
 import java.io.File;
@@ -14,6 +15,7 @@ import org.generationcp.middleware.pojos.Person;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -32,8 +34,8 @@ public class CrossesListDescriptionSheetParserTest {
 	@Mock
 	private UserDataManager userDataManager;
 
-	private CrossesListDescriptionSheetParser<ImportedCrossesList>
-			crossesListDescriptionSheetParser = new CrossesListDescriptionSheetParser<>(this.crossesList, this.userDataManager);
+	private CrossesListDescriptionSheetParser<ImportedCrossesList> crossesListDescriptionSheetParser =
+			new CrossesListDescriptionSheetParser<>(this.crossesList, this.userDataManager);
 
 	private Workbook workbookNoListDate;
 	private Workbook workbook;
@@ -43,19 +45,20 @@ public class CrossesListDescriptionSheetParserTest {
 	public void setUp() throws Exception {
 		final Person personTest = new Person("Test", "Test", "Test");
 		personTest.setId(1);
-		Mockito.when(this.userDataManager.getPersonByName(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
-						.thenReturn(personTest);
+		Mockito.when(this.userDataManager.getPersonByName(Matchers.anyString(), Matchers.anyString(), Matchers.anyString()))
+				.thenReturn(personTest);
 
 		this.crossesListDescriptionSheetParser = new CrossesListDescriptionSheetParser<>(this.crossesList, this.userDataManager);
 
 		this.today = new Date();
-		final URL crossesListWithoutDateURL = ClassLoader.getSystemClassLoader().getResource(CROSSES_LIST_NO_LIST_DATE);
+		final URL crossesListWithoutDateURL =
+				ClassLoader.getSystemClassLoader().getResource(CrossesListDescriptionSheetParserTest.CROSSES_LIST_NO_LIST_DATE);
 		if (crossesListWithoutDateURL != null) {
 			final File workbookFile1 = new File(crossesListWithoutDateURL.toURI());
 			assert workbookFile1.exists();
 			this.workbookNoListDate = WorkbookFactory.create(workbookFile1);
 		}
-		final URL crossesListURL = ClassLoader.getSystemClassLoader().getResource(CROSSES_LIST);
+		final URL crossesListURL = ClassLoader.getSystemClassLoader().getResource(CrossesListDescriptionSheetParserTest.CROSSES_LIST);
 		if (crossesListURL != null) {
 			final File workbookFile2 = new File(crossesListURL.toURI());
 			assert workbookFile2.exists();
@@ -66,19 +69,24 @@ public class CrossesListDescriptionSheetParserTest {
 	@Test
 	public void testEmptyListDate() throws ParseException, FileParsingException {
 		this.crossesListDescriptionSheetParser.parseWorkbook(this.workbookNoListDate, null);
-		Assert.assertTrue("The list date should be " + this.today, this.crossesListDescriptionSheetParser.getImportedList().getDate().after(this.today));
+		Assert.assertTrue("The list date should be " + this.today,
+				this.crossesListDescriptionSheetParser.getImportedList().getDate().after(this.today));
 	}
 
 	@Test
 	public void testListType() throws ParseException, FileParsingException {
 		this.crossesListDescriptionSheetParser.parseWorkbook(this.workbookNoListDate, null);
-		Assert.assertTrue("The list type should be " + CrossesListDescriptionSheetParser.TEMPLATE_LIST_TYPE, this.crossesListDescriptionSheetParser.getImportedList().getType().equals(CrossesListDescriptionSheetParser.TEMPLATE_LIST_TYPE));
+		Assert.assertTrue("The list type should be " + CrossesListDescriptionSheetParser.TEMPLATE_LIST_TYPE,
+				this.crossesListDescriptionSheetParser.getImportedList().getType()
+						.equals(CrossesListDescriptionSheetParser.TEMPLATE_LIST_TYPE));
 	}
 
 	@Test
 	public void testWithListDate() throws ParseException, FileParsingException {
 		this.crossesListDescriptionSheetParser.parseWorkbook(this.workbook, null);
-		Assert.assertTrue("The list date should be " + LIST_DATE_IN_XLS_TEST_FILE, this.crossesListDescriptionSheetParser.getImportedList().getDate().equals(DateUtil.parseDate(LIST_DATE_IN_XLS_TEST_FILE)));
+		Assert.assertTrue("The list date should be " + CrossesListDescriptionSheetParserTest.LIST_DATE_IN_XLS_TEST_FILE,
+				this.crossesListDescriptionSheetParser.getImportedList().getDate()
+						.equals(DateUtil.parseDate(CrossesListDescriptionSheetParserTest.LIST_DATE_IN_XLS_TEST_FILE)));
 	}
 
 }
