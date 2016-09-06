d3e5d4b47ce0b8cd3a9feb83370083b02472789b
Akhil Mehra <akhil@leafnode.io>
2016-09-06 15:38:11 +1200

    Documentation, Naming and Test improvements
    1) Renamed TransformationUtil to CollectionTransformationUtil
    2) Added Javadoc
    3) Added tests
    
    issue: BMS-3080
    reviewer: none

--- src/main/java/org/generationcp/commons/util/CollectionTransformationUtil.java
+++ src/main/java/org/generationcp/commons/util/CollectionTransformationUtil.java
@@ -1,71 +1,93 @@
+
 package org.generationcp.commons.util;
 
 import java.util.Collection;
 import java.util.List;
 
 import org.generationcp.middleware.pojos.Germplasm;
-import org.generationcp.middleware.pojos.GermplasmListData;
 
 import com.google.common.base.Function;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
+
 /**
- * 
- * @author Akhil
+ * A utility that helps transform from one collection to another.
  *
  */
-public class TransformationUtil {
+public class CollectionTransformationUtil {
 
-	private TransformationUtil() {
-		
+	private CollectionTransformationUtil() {
+
 	}
 
 	/**
-	 * Warning exception with list on uniques
-	 * @param germplasmList
-	 * @return
+	 * Helps transform {@link Germplasm} list into a map. Warning am exception is thrown when the list has non unique values
+	 * 
+	 * @param germplasmList the list of germplasm to transform
+	 * @return the resultant map.
 	 */
 	public static ImmutableMap<Integer, Germplasm> getGermplasmMap(final List<Germplasm> germplasmList) {
 		return Maps.uniqueIndex(germplasmList, new Function<Germplasm, Integer>() {
+
 			public Integer apply(Germplasm from) {
 				return from.getGid(); // or something else
 			}
 		});
 	}
-	
-	public static ImmutableSet<Integer> getAllGidsFromGermplasmListDataList(final Collection<GermplasmListData> germplasms) {
-		final Function<GermplasmListData, Integer> getGidFunction = new Function<GermplasmListData, Integer>() {
-			public Integer apply(GermplasmListData germplasmListEntry) {
-				return germplasmListEntry.getGid();
-			}
-		};
-		return FluentIterable.from(germplasms).transform(getGidFunction).toSet();
-	}
 
+	/**
+	 * Helps retrieve a list of germplasm id from a collection of {@link Germplasm}
+	 * 
+	 * @param germplasms the list of germplasm to mine
+	 * @return the resultant list of germplasm ids.
+	 */
 	public static ImmutableSet<Integer> getAllGidsFromGermplasmList(final Collection<Germplasm> germplasms) {
 		final Function<Germplasm, Integer> getGidFunction = new Function<Germplasm, Integer>() {
+
 			public Integer apply(Germplasm germplasmListEntry) {
 				return germplasmListEntry.getGid();
 			}
 		};
 		return FluentIterable.from(germplasms).transform(getGidFunction).toSet();
 	}
-	
+
+	/**
+	 * Helps retrieve a list of germplasm female parent ids from a collection of {@link Germplasm}
+	 * 
+	 * @param germplasms the list of germplasm to mine
+	 * @return the resultant list of germplasm female parent ids.
+	 */
 	public static ImmutableSet<Integer> getAllFemaleParentGidsFromGermplasmList(final Collection<Germplasm> germplasms) {
 		final Function<Germplasm, Integer> getGidFunction = new Function<Germplasm, Integer>() {
+
 			public Integer apply(Germplasm germplasmListEntry) {
-				return germplasmListEntry.getGpid1();
+				final Integer gpid1 = germplasmListEntry.getGpid1();
+				if (gpid1 == null) {
+					return 0;
+				}
+				return gpid1;
 			}
 		};
 		return FluentIterable.from(germplasms).transform(getGidFunction).toSet();
 	}
 
+	/**
+	 * Helps retrieve a list of germplasm male parent ids from a collection of {@link Germplasm}
+	 * 
+	 * @param germplasms the list of germplasm to mine
+	 * @return the resultant list of germplasm female parent ids.
+	 */
 	public static ImmutableSet<Integer> getAllMaleParentGidsFromGermplasmList(final Collection<Germplasm> germplasms) {
 		final Function<Germplasm, Integer> getGidFunction = new Function<Germplasm, Integer>() {
+
 			public Integer apply(Germplasm germplasmListEntry) {
-				return germplasmListEntry.getGpid2();
+				final Integer gpid2 = germplasmListEntry.getGpid2();
+				if (gpid2 == null) {
+					return 0;
+				}
+				return gpid2;
 			}
 		};
 		return FluentIterable.from(germplasms).transform(getGidFunction).toSet();
