73e93985fbbfb15cdb881b643b204531c65405e0
Naymesh <naymesh@leafnode.io>
2016-09-07 17:04:43 +1200

    Reverting the pre-authentication process to correct logic:  only rely on authToken, nothing else, no fallbacks.
    More work needed to test and fix the navigation issue that was originally addressed for  GCP-10518,  which caused this security issue.
    Note that this change is just a starting point. Other fixes will be needed as we test the impact of this on overall security/pre-auth process. I expect more changes/fixes in other areas will still be needed.
    
    Issue: BMS-1986
    Reviewer: MatthewB

--- src/main/java/org/generationcp/commons/security/BMSPreAuthenticationFilter.java
+++ src/main/java/org/generationcp/commons/security/BMSPreAuthenticationFilter.java
@@ -1,48 +1,33 @@
 
 package org.generationcp.commons.security;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.generationcp.commons.util.ContextUtil;
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
-import org.generationcp.middleware.manager.api.WorkbenchDataManager;
+import org.generationcp.commons.context.ContextConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedGrantedAuthoritiesWebAuthenticationDetails;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
 
 public class BMSPreAuthenticationFilter extends AbstractPreAuthenticatedProcessingFilter {
 
 	private static final Logger LOG = LoggerFactory.getLogger(BMSPreAuthenticationFilter.class);
-	
-	@Resource
-	WorkbenchDataManager workbenchDataManager;
 
-	@Autowired
-	PlatformTransactionManager transactionManager;
-	
 	@Override
 	protected Object getPreAuthenticatedPrincipal(final HttpServletRequest request) {
-		TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-
-		return transactionTemplate.execute(new TransactionCallback<Object>() {
-			@Override
-			public Object doInTransaction(TransactionStatus status) {
-				try {
-					return ContextUtil.getCurrentWorkbenchUsername(BMSPreAuthenticationFilter.this.workbenchDataManager, request);
-				} catch (MiddlewareQueryException e) {
-					BMSPreAuthenticationFilter.LOG.error(e.getMessage(), e);
-				}
-				// TODO Auto-generated method stub
-				return null;
-			}
-		});
+		/**
+		 * Pre-authenticate IF AND ONLY IF there is authToken provided. No other fall backs such as use of ContextUtil should be allowed. We
+		 * must reject all requests where no authToken parameter is present. Returning null ensures this happens.
+		 * 
+		 * WARNING for newbie developers : DO NOT change any logic here else a big security hole will be opened up. Contact a senior
+		 * developer in Team NZ before making any changes to this class.
+		 */
+		String authToken = request.getParameter(ContextConstants.PARAM_AUTH_TOKEN);
+		if (authToken != null) {
+			String principal = SecurityUtil.decodeToken(authToken);
+			return principal;
+		}
+		LOG.info("Could not pre-authenticate the request: {}. No authentication token was supplied.", request.getRequestURL());
+		return null;
 	}
 
 	@Override
