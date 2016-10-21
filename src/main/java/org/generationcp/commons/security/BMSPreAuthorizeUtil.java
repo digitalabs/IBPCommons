ab9caf524a573eec816bb4050070ce5ae28c3d8a
abhishekptl <abhishek@leafnode.io>
2016-10-21 11:37:55 +0530

    Adding utility method to authorize access based on passed roles and logged in user role
    This utility method will called wherever restriction on link is required.
    
    issue: BMS-3488
    reviewer: Sunny

---
+++ src/main/java/org/generationcp/commons/security/BMSPreAuthorizeUtil.java
@@ -0,0 +1,32 @@
+package org.generationcp.commons.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.generationcp.middleware.util.StringUtil;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.google.common.collect.Lists;
+
+public class BMSPreAuthorizeUtil {
+
+	public static void preAuthorize(final String configuredRoles) {
+
+		final Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		final List<String> permittedRoleList = Lists.newArrayList(configuredRoles.split(","));
+
+		if (permittedRoleList.isEmpty()) {
+			throw new AccessDeniedException("No role is configured to access this link");
+		}
+
+		for (final GrantedAuthority grantedAuthority : authorities) {
+			final String authority = grantedAuthority.getAuthority();
+			if (!StringUtil.containsIgnoreCase(permittedRoleList, authority.split(SecurityUtil.ROLE_PREFIX)[1])) {
+				throw new AccessDeniedException("You have not authorized role to access this link");
+			}
+
+		}
+	}
+}
