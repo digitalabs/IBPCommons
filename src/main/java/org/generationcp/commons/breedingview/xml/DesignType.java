b9ac1f8b012525677b28f5b9f8d465cb0c7ed3c5
Aldrin Batac <aldrin@leafnode.io>
2016-10-25 11:58:35 +0800

    Cleanup: Applied auto formatting
    BMS-2998

--- src/main/java/org/generationcp/commons/breedingview/xml/DesignType.java
+++ src/main/java/org/generationcp/commons/breedingview/xml/DesignType.java
@@ -1,28 +1,26 @@
 /***************************************************************
  * Copyright (c) 2012, All Rights Reserved.
- *
+ * <p/>
  * Generation Challenge Programme (GCP)
  *
  * @author Kevin L. Manansala
- *
- *         This software is licensed for use under the terms of the GNU General Public License (http://bit.ly/8Ztv8M) and the provisions of
- *         Part F of the Generation Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
- *
+ * <p/>
+ * This software is licensed for use under the terms of the GNU General Public License (http://bit.ly/8Ztv8M) and the provisions of
+ * Part F of the Generation Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
  **************************************************************/
 
 package org.generationcp.commons.breedingview.xml;
 
 public enum DesignType {
 	RANDOMIZED_BLOCK_DESIGN("Randomized block design"), RESOLVABLE_INCOMPLETE_BLOCK_DESIGN(
-			"Resolvable incomplete block design"), RESOLVABLE_ROW_COLUMN_DESIGN(
-			"Resolvable row-column design"), P_REP_DESIGN("P-rep design"),
-			// Augmented design is just a variation of the Incomplete Block Design type.
-			AUGMENTED_RANDOMIZED_BLOCK("Incomplete block design");
+			"Resolvable incomplete block design"), RESOLVABLE_ROW_COLUMN_DESIGN("Resolvable row-column design"), P_REP_DESIGN(
+			"P-rep design"),
+	// Augmented design is just a variation of the Incomplete Block Design type.
+	AUGMENTED_RANDOMIZED_BLOCK("Incomplete block design");
 
 	private final String name;
 
-
-	private DesignType(String name) {
+	private DesignType(final String name) {
 		this.name = name;
 	}
 
@@ -38,10 +36,10 @@ public enum DesignType {
 	 * Gets the design type by name.
 	 * @param name
 	 * @return
-     */
-	public static DesignType getDesignTypeByName(String name) {
+	 */
+	public static DesignType getDesignTypeByName(final String name) {
 
-		for (DesignType designType: DesignType.values()){
+		for (final DesignType designType : DesignType.values()) {
 			if (designType.getName().equals(name)) {
 				return designType;
 			}
--- src/test/java/org/generationcp/commons/breedingview/xml/DesignTypeTest.java
+++ src/test/java/org/generationcp/commons/breedingview/xml/DesignTypeTest.java
@@ -5,24 +5,26 @@ import org.junit.Test;
 
 public class DesignTypeTest {
 
+	@Test
+	public void testDesignTypeFromString() {
 
-    @Test
-    public void testDesignTypeFromString() {
+		Assert.assertEquals(DesignType.RANDOMIZED_BLOCK_DESIGN,
+				DesignType.getDesignTypeByName(DesignType.RANDOMIZED_BLOCK_DESIGN.getName()));
+		Assert.assertEquals(DesignType.RESOLVABLE_INCOMPLETE_BLOCK_DESIGN,
+				DesignType.getDesignTypeByName(DesignType.RESOLVABLE_INCOMPLETE_BLOCK_DESIGN.getName()));
+		Assert.assertEquals(DesignType.RESOLVABLE_ROW_COLUMN_DESIGN,
+				DesignType.getDesignTypeByName(DesignType.RESOLVABLE_ROW_COLUMN_DESIGN.getName()));
+		Assert.assertEquals(DesignType.P_REP_DESIGN, DesignType.getDesignTypeByName(DesignType.P_REP_DESIGN.getName()));
+		Assert.assertEquals(DesignType.AUGMENTED_RANDOMIZED_BLOCK,
+				DesignType.getDesignTypeByName(DesignType.AUGMENTED_RANDOMIZED_BLOCK.getName()));
+	}
 
-       Assert.assertEquals(DesignType.RANDOMIZED_BLOCK_DESIGN, DesignType.getDesignTypeByName(DesignType.RANDOMIZED_BLOCK_DESIGN.getName()));
-       Assert.assertEquals(DesignType.RESOLVABLE_INCOMPLETE_BLOCK_DESIGN, DesignType.getDesignTypeByName(DesignType.RESOLVABLE_INCOMPLETE_BLOCK_DESIGN.getName()));
-       Assert.assertEquals(DesignType.RESOLVABLE_ROW_COLUMN_DESIGN, DesignType.getDesignTypeByName(DesignType.RESOLVABLE_ROW_COLUMN_DESIGN.getName()));
-       Assert.assertEquals(DesignType.P_REP_DESIGN, DesignType.getDesignTypeByName(DesignType.P_REP_DESIGN.getName()));
-       Assert.assertEquals(DesignType.AUGMENTED_RANDOMIZED_BLOCK, DesignType.getDesignTypeByName(DesignType.AUGMENTED_RANDOMIZED_BLOCK.getName()));
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void testDesignTypeFromStringException() {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testDesignTypeFromStringException() {
+		// This will throw an exception because an empty string doesn't match any of the enum constants in DesignType.
+		DesignType.getDesignTypeByName("");
 
-        // This will throw an exception because an empty string doesn't match any of the enum constants in DesignType.
-        DesignType.getDesignTypeByName("");
-
-    }
-
+	}
 
 }
