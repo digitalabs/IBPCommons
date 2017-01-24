9669a093c149cc9628df8d1c4f4d7521eec48bb9
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2017-01-25 01:27:35 +0800

    Added new enum values of the inventory and planting label printing presets, and removed the FBK_LABEL_PRINTING

--- src/main/java/org/generationcp/commons/constant/ToolSection.java
+++ src/main/java/org/generationcp/commons/constant/ToolSection.java
@@ -5,5 +5,9 @@ package org.generationcp.commons.constant;
  * Created by cyrus on 12/19/14. Add specific tool sections here For now, this will be used specifically in presets
  */
 public enum ToolSection {
-	FBK_LABEL_PRINTING, FBK_CROSS_IMPORT, FB_LBL_PRINT_CUSTOM_REPORT, FB_NURSE_MGR_CUSTOM_REPORT, FB_TRIAL_MGR_CUSTOM_REPORT, BM_LIST_MGR_CUSTOM_REPORT
+	// Jasper Reports
+	FBK_CROSS_IMPORT, FB_LBL_PRINT_CUSTOM_REPORT, FB_NURSE_MGR_CUSTOM_REPORT, FB_TRIAL_MGR_CUSTOM_REPORT, BM_LIST_MGR_CUSTOM_REPORT,
+	
+	// Label printing presets
+	INVENTORY_LABEL_PRINTING_PRESET, PLANTING_LABEL_PRINTING_PRESET
 }
