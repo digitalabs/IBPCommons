f5c7ea4fc0befe2ef2b64fda64abd32064d89aa6
Clarysabel Tovar <clarysabel@leafnode.io>
2017-01-25 16:21:23 -0300

    Added basedOnImportFile attribute to BreedingMethodSetting Issue: BMS-3786 Reviewer: none

--- src/main/java/org/generationcp/commons/settings/BreedingMethodSetting.java
+++ src/main/java/org/generationcp/commons/settings/BreedingMethodSetting.java
@@ -13,15 +13,17 @@ public class BreedingMethodSetting implements Serializable {
 
 	private Integer methodId;
 	private boolean isBasedOnStatusOfParentalLines;
+  	private boolean basedOnImportFile;
 
 	public BreedingMethodSetting() {
 
 	}
 
-	public BreedingMethodSetting(Integer methodId, boolean isBasedOnStatusOfParentalLines) {
+	public BreedingMethodSetting(Integer methodId, boolean isBasedOnStatusOfParentalLines, boolean basedOnImportFile) {
 		super();
 		this.methodId = methodId;
 		this.isBasedOnStatusOfParentalLines = isBasedOnStatusOfParentalLines;
+	  	this.basedOnImportFile = basedOnImportFile;
 	}
 
 	@XmlAttribute
@@ -42,7 +44,16 @@ public class BreedingMethodSetting implements Serializable {
 		this.isBasedOnStatusOfParentalLines = isBasedOnStatusOfParentalLines;
 	}
 
-	@Override
+	@XmlAttribute
+	public boolean isBasedOnImportFile() {
+	  return basedOnImportFile;
+	}
+
+	public void setBasedOnImportFile(final boolean basedOnImportFile) {
+	  this.basedOnImportFile = basedOnImportFile;
+	}
+
+    @Override
 	public boolean equals(Object obj) {
 		if (obj == null) {
 			return false;
@@ -56,7 +67,8 @@ public class BreedingMethodSetting implements Serializable {
 
 		BreedingMethodSetting rhs = (BreedingMethodSetting) obj;
 		return new EqualsBuilder().append(this.methodId, rhs.methodId)
-				.append(this.isBasedOnStatusOfParentalLines, rhs.isBasedOnStatusOfParentalLines).isEquals();
+				.append(this.isBasedOnStatusOfParentalLines, rhs.isBasedOnStatusOfParentalLines)
+				.append(this.basedOnImportFile, rhs.isBasedOnImportFile()).isEquals();
 	}
 
 	@Override
@@ -64,6 +76,7 @@ public class BreedingMethodSetting implements Serializable {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + (this.isBasedOnStatusOfParentalLines ? 1231 : 1237);
+  	    result = prime * result + (this.basedOnImportFile ? 1231 : 1237);
 		result = prime * result + (this.methodId == null ? 0 : this.methodId.hashCode());
 		return result;
 	}
@@ -71,7 +84,7 @@ public class BreedingMethodSetting implements Serializable {
 	@Override
 	public String toString() {
 		return "BreedingMethodSetting [methodId=" + this.methodId + ", isBasedOnStatusOfParentalLines="
-						+ this.isBasedOnStatusOfParentalLines + "]";
+						+ this.isBasedOnStatusOfParentalLines + ", basedOnImportFile=" + this.basedOnImportFile + "]";
 	}
 
 }
