34290704570856be87e2831e7442edb0ddcc03c5
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2017-02-13 09:46:45 +0800

    Applied leafnode clean up
    BMS-3807

--- src/test/java/org/generationcp/commons/util/MySQLUtilTest.java
+++ src/test/java/org/generationcp/commons/util/MySQLUtilTest.java
@@ -19,15 +19,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:/testContext.xml"})
+@ContextConfiguration(locations = { "classpath*:/testContext.xml" })
 public class MySQLUtilTest {
 	private static final String DATABASE = "ibdbv2_maize_merged";
 	private static final String BACKUP_FILENAME = "C:\\GCP\\Commons\\temp\\ibdbv2_maize_merged_20170209_033042_735_.sql";
 
 	private Project project;
-	
+
 	private ProjectTestDataInitializer projectTestDataInitializer;
-	
+
 	@Autowired
 	@InjectMocks
 	private MySQLUtil mysqlUtil;
@@ -38,62 +38,75 @@ public class MySQLUtilTest {
 		this.projectTestDataInitializer = new ProjectTestDataInitializer();
 		this.project = this.projectTestDataInitializer.createProject();
 	}
-	
+
 	@Test
 	public void testBuildProjectQueryString() {
-		final String projectQueryString = this.mysqlUtil.buildProjectQueryString(project).toString();
-		final String expectedQueryString = "INSERT into `workbench_project` (`project_id`, `user_id`, `project_name`, `start_date`, `project_uuid`, `crop_type`, `last_open_date`) values (null, 9999, '" + project.getProjectName() + "','" + project.getStartDate() + "','" + project.getUniqueID() + "','tutorial','" + project.getLastOpenDate() + "');\n";
+		final String projectQueryString = this.mysqlUtil.buildProjectQueryString(this.project).toString();
+		final String expectedQueryString = "INSERT into `workbench_project` (`project_id`, `user_id`, `project_name`, `start_date`, `project_uuid`, `crop_type`, `last_open_date`) values (null, 9999, '"
+				+ this.project.getProjectName() + "','" + this.project.getStartDate() + "','"
+				+ this.project.getUniqueID() + "','tutorial','" + this.project.getLastOpenDate() + "');\n";
 		Assert.assertEquals(expectedQueryString, projectQueryString);
 	}
-	
+
 	@Test
-	public void testbuildCommandStringListIncludeProceduresAndWithPassword(){
+	public void testbuildCommandStringListIncludeProceduresAndWithPassword() {
 		final String password = "password";
 		this.mysqlUtil.setPassword(password);
-		List<String> commandStringList = this.mysqlUtil.buildCommandStringList(DATABASE, BACKUP_FILENAME, true);
-		
-		final String mysqlDumpAbsolutePath = new File(this.mysqlUtil.getMysqlDumpPath()).getAbsolutePath();;
-		final List<String> expectedCommandList = new ArrayList<String>(Arrays.asList(mysqlDumpAbsolutePath, "--password=" + password, "--routines", "--complete-insert", "--extended-insert",
-				"--no-create-db", "--single-transaction", "--default-character-set=utf8", "--host=" + this.mysqlUtil.getMysqlHost(),
-				"--port=" + this.mysqlUtil.getMysqlPort(), "--user=" + this.mysqlUtil.getUsername(), DATABASE, "-r", BACKUP_FILENAME));
-		
-		int i=0;
-		for(String value: commandStringList){
-			final String expectedValue = expectedCommandList.get(i++);
-			Assert.assertEquals("The value should be " + expectedValue, expectedValue, value);
-		}
-	}
-	
-	@Test
-	public void testbuildCommandStringListWithPassword(){
-		final String password = "password";
-		this.mysqlUtil.setPassword(password);
-		List<String> commandStringList = this.mysqlUtil.buildCommandStringList(DATABASE, BACKUP_FILENAME, false);
-		
-		final String mysqlDumpAbsolutePath = new File(this.mysqlUtil.getMysqlDumpPath()).getAbsolutePath();;
-		final List<String> expectedCommandList = new ArrayList<String>(Arrays.asList(mysqlDumpAbsolutePath, "--password=" + password, "--complete-insert", "--extended-insert",
-				"--no-create-db", "--single-transaction", "--default-character-set=utf8", "--host=" + this.mysqlUtil.getMysqlHost(),
-				"--port=" + this.mysqlUtil.getMysqlPort(), "--user=" + this.mysqlUtil.getUsername(), DATABASE, "-r", BACKUP_FILENAME));
-		
-		int i=0;
-		for(String value: commandStringList){
-			final String expectedValue = expectedCommandList.get(i++);
-			Assert.assertEquals("The value should be " + expectedValue, expectedValue, value);
-		}
-	}
-	
-	@Test
-	public void testbuildCommandStringListIncludeProceduresAndNoPassword(){
-		this.mysqlUtil.setPassword("");
-		List<String> commandStringList = this.mysqlUtil.buildCommandStringList(DATABASE, BACKUP_FILENAME, true);
-		
+		final List<String> commandStringList = this.mysqlUtil.buildCommandStringList(MySQLUtilTest.DATABASE,
+				MySQLUtilTest.BACKUP_FILENAME, true);
+
 		final String mysqlDumpAbsolutePath = new File(this.mysqlUtil.getMysqlDumpPath()).getAbsolutePath();
-		final List<String> expectedCommandList = new ArrayList<String>(Arrays.asList(mysqlDumpAbsolutePath, "--routines", "--complete-insert", "--extended-insert",
-				"--no-create-db", "--single-transaction", "--default-character-set=utf8", "--host=" + this.mysqlUtil.getMysqlHost(),
-				"--port=" + this.mysqlUtil.getMysqlPort(), "--user=" + this.mysqlUtil.getUsername(), DATABASE, "-r", BACKUP_FILENAME));
-		
-		int i=0;
-		for(String value: commandStringList){
+		;
+		final List<String> expectedCommandList = new ArrayList<String>(Arrays.asList(mysqlDumpAbsolutePath,
+				"--password=" + password, "--routines", "--complete-insert", "--extended-insert", "--no-create-db",
+				"--single-transaction", "--default-character-set=utf8", "--host=" + this.mysqlUtil.getMysqlHost(),
+				"--port=" + this.mysqlUtil.getMysqlPort(), "--user=" + this.mysqlUtil.getUsername(),
+				MySQLUtilTest.DATABASE, "-r", MySQLUtilTest.BACKUP_FILENAME));
+
+		int i = 0;
+		for (final String value : commandStringList) {
+			final String expectedValue = expectedCommandList.get(i++);
+			Assert.assertEquals("The value should be " + expectedValue, expectedValue, value);
+		}
+	}
+
+	@Test
+	public void testbuildCommandStringListWithPassword() {
+		final String password = "password";
+		this.mysqlUtil.setPassword(password);
+		final List<String> commandStringList = this.mysqlUtil.buildCommandStringList(MySQLUtilTest.DATABASE,
+				MySQLUtilTest.BACKUP_FILENAME, false);
+
+		final String mysqlDumpAbsolutePath = new File(this.mysqlUtil.getMysqlDumpPath()).getAbsolutePath();
+		;
+		final List<String> expectedCommandList = new ArrayList<String>(Arrays.asList(mysqlDumpAbsolutePath,
+				"--password=" + password, "--complete-insert", "--extended-insert", "--no-create-db",
+				"--single-transaction", "--default-character-set=utf8", "--host=" + this.mysqlUtil.getMysqlHost(),
+				"--port=" + this.mysqlUtil.getMysqlPort(), "--user=" + this.mysqlUtil.getUsername(),
+				MySQLUtilTest.DATABASE, "-r", MySQLUtilTest.BACKUP_FILENAME));
+
+		int i = 0;
+		for (final String value : commandStringList) {
+			final String expectedValue = expectedCommandList.get(i++);
+			Assert.assertEquals("The value should be " + expectedValue, expectedValue, value);
+		}
+	}
+
+	@Test
+	public void testbuildCommandStringListIncludeProceduresAndNoPassword() {
+		this.mysqlUtil.setPassword("");
+		final List<String> commandStringList = this.mysqlUtil.buildCommandStringList(MySQLUtilTest.DATABASE,
+				MySQLUtilTest.BACKUP_FILENAME, true);
+
+		final String mysqlDumpAbsolutePath = new File(this.mysqlUtil.getMysqlDumpPath()).getAbsolutePath();
+		final List<String> expectedCommandList = new ArrayList<String>(Arrays.asList(mysqlDumpAbsolutePath,
+				"--routines", "--complete-insert", "--extended-insert", "--no-create-db", "--single-transaction",
+				"--default-character-set=utf8", "--host=" + this.mysqlUtil.getMysqlHost(),
+				"--port=" + this.mysqlUtil.getMysqlPort(), "--user=" + this.mysqlUtil.getUsername(),
+				MySQLUtilTest.DATABASE, "-r", MySQLUtilTest.BACKUP_FILENAME));
+
+		int i = 0;
+		for (final String value : commandStringList) {
 			final String expectedValue = expectedCommandList.get(i++);
 			Assert.assertEquals("The value should be " + expectedValue, expectedValue, value);
 		}
