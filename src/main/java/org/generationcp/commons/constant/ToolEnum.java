a7985b567a182370c50130ebe13366087a6a8e98
cuenyad <cuenya.diego@gmail.com>
2018-02-22 17:25:34 -0300

    Adding study_manager_fieldbook_web
    ISSUE BMS-4710
    Reviewer None

--- src/main/java/org/generationcp/commons/constant/ToolEnum.java
+++ src/main/java/org/generationcp/commons/constant/ToolEnum.java
@@ -12,7 +12,7 @@ public enum ToolEnum {
 			"breeding_planner"), IBFB_GERMPLASM_IMPORT("ibfb_germplasm_import"), GERMPLASM_IMPORT("germplasm_import"), HEAD_TO_HEAD_BROWSER(
 			"germplasm_headtohead"), MAIN_HEAD_TO_HEAD_BROWSER("germplasm_mainheadtohead"), DATASET_IMPORTER("dataset_importer"), QUERY_FOR_ADAPTED_GERMPLASM(
 			"query_for_adapted_germplasm"), TRAIT_DONOR_QUERY("trait_donor_query"), FIELDBOOK_WEB("fieldbook_web"), NURSERY_MANAGER_FIELDBOOK_WEB(
-			"nursery_manager_fieldbook_web"), TRIAL_MANAGER_FIELDBOOK_WEB("trial_manager_fieldbook_web"), ONTOLOGY_BROWSER_FIELDBOOK_WEB(
+			"nursery_manager_fieldbook_web"), TRIAL_MANAGER_FIELDBOOK_WEB("trial_manager_fieldbook_web"), STUDY_MANAGER_FIELDBOOK_WEB("study_manager_fieldbook_web"), ONTOLOGY_BROWSER_FIELDBOOK_WEB(
 			"ontology_browser_fieldbook_web"), MIGRATOR("migrator"), ONTOLOGY_MANAGER("ontology_manager");
 
 	String toolName;
