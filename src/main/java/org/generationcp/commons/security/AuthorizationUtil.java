107dd36f2bc849f7a8ad86025c794c67b77cba2b
Darla Hermo <darla@leafnode.io>
2018-06-05 09:59:39 +0800

    Changed pre-authorization parameter from a string of delimited role names to list of roles
    BMS-4932

--- src/main/java/org/generationcp/commons/security/AuthorizationUtil.java
+++ src/main/java/org/generationcp/commons/security/AuthorizationUtil.java
@@ -1,33 +1,37 @@
 package org.generationcp.commons.security;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
+import org.fest.util.Collections;
+import org.generationcp.middleware.pojos.workbench.Role;
 import org.generationcp.middleware.util.StringUtil;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.GrantedAuthority;
 
-import com.google.common.collect.Lists;
-
 public class AuthorizationUtil {
 
-	public static void preAuthorize(final String configuredRoles) {
+	public static void preAuthorize(final List<Role> configuredRoles) {
 
 		final Collection<? extends GrantedAuthority> authorities = SecurityUtil.getLoggedInUserRoles();
 
-		if (StringUtils.isEmpty(configuredRoles)) {
+		if (Collections.isNullOrEmpty(configuredRoles)) {
 			throw new AccessDeniedException("Access Denied. No role is configured to access this functionality.");
 		}
-
-		final List<String> permittedRoleList = Lists.newArrayList(configuredRoles.split(","));
-
+		final List<String> permittedRoleNames = new ArrayList<>();
+		for (final Role role : configuredRoles) {
+			permittedRoleNames.add(role.getName());
+		}
+		
 		for (final GrantedAuthority grantedAuthority : authorities) {
 			final String authority = grantedAuthority.getAuthority();
-			if (!StringUtil.containsIgnoreCase(permittedRoleList, authority.split(SecurityUtil.ROLE_PREFIX)[1])) {
+			if (!StringUtil.containsIgnoreCase(permittedRoleNames, authority.split(SecurityUtil.ROLE_PREFIX)[1])) {
 				throw new AccessDeniedException("Access Denied. User does not have appropriate role to access the functionality.");
 			}
 
 		}
 	}
+	
+	
 }
