4320e0d9b0849a910cde773ad36dce01e53e3325
Darla Hermo <darla@leafnode.io>
2018-09-27 13:25:26 +0800

    Fix bug in checking if role is in list of roles. Perform whole word checking on role names instead of using StringUtil.containsIgnoreCase so that "ADMIN" will not be returned as authorized for "SUPERADMIN" role
    BMS-5185

--- src/main/java/org/generationcp/commons/security/AuthorizationUtil.java
+++ src/main/java/org/generationcp/commons/security/AuthorizationUtil.java
@@ -7,12 +7,14 @@ import java.util.List;
 
 import org.fest.util.Collections;
 import org.generationcp.middleware.pojos.workbench.Role;
-import org.generationcp.middleware.util.StringUtil;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.GrantedAuthority;
 
-public class AuthorizationUtil {
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 
+public class AuthorizationUtil {
+	
 	protected static final String ACCESS_DENIED_MESSAGE = "Access Denied. User does not have appropriate role to access the functionality.";
 
 	public static void preAuthorize(final List<Role> configuredRoles) {
@@ -29,9 +31,17 @@ public class AuthorizationUtil {
 	
 	private static void preAuthorizeUser(final List<String> roleNames){
 		final Collection<? extends GrantedAuthority> authorities = SecurityUtil.getLoggedInUserRoles();
+		final List<String> capitalizedRoles = Lists.transform(roleNames, new Function<String, String>() {
+			@Override
+			public String apply(final String role) {
+				return role.toUpperCase();
+			}
+		});
+		
 		for (final GrantedAuthority grantedAuthority : authorities) {
 			final String authority = grantedAuthority.getAuthority();
-			if (!StringUtil.containsIgnoreCase(roleNames, authority.split(SecurityUtil.ROLE_PREFIX)[1])) {
+			final String roleName = authority.split(SecurityUtil.ROLE_PREFIX)[1].toUpperCase();
+			if (!capitalizedRoles.contains(roleName)) {
 				throw new AccessDeniedException(ACCESS_DENIED_MESSAGE);
 			}
 
