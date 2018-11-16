0d1cd1844def4948a134a73825b5dc7987e8e834
Aldrin Batac <aldrin.batac@leafnode.io>
2018-11-16 13:29:04 +0800

    Code cleanup.
    IBP-1922

--- src/main/java/org/generationcp/commons/util/ZipUtil.java
+++ src/main/java/org/generationcp/commons/util/ZipUtil.java
@@ -4,7 +4,6 @@ package org.generationcp.commons.util;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,33 +24,31 @@ public class ZipUtil {
 	public static final String ZIP_EXTENSION = ".zip";
 	private static final int BUFFER_SIZE = 4096;
 	private static final Logger LOG = LoggerFactory.getLogger(ZipUtil.class);
-	
-	final private InstallationDirectoryUtil installationDirectoryUtil = new InstallationDirectoryUtil();
+
+	private final InstallationDirectoryUtil installationDirectoryUtil = new InstallationDirectoryUtil();
 
 	public File zipFiles(final List<File> files)
 		throws IOException {
 		final byte[] buffer = new byte[1024];
 
 		final String zipPath = TEMP_FILE_DIR + UUID.randomUUID() + ZIP_EXTENSION;
-		FileOutputStream fos = new FileOutputStream(zipPath);
+		final FileOutputStream fos = new FileOutputStream(zipPath);
 
 		final ZipOutputStream zos = new ZipOutputStream(fos);
 
 		try {
 
-			for (File file : files) {
+			for (final File file : files) {
 
-				ZipEntry ze = new ZipEntry(file.getName());
+				final ZipEntry ze = new ZipEntry(file.getName());
 				zos.putNextEntry(ze);
 
-				FileInputStream in = new FileInputStream(file);
-
-				int len;
-				while ((len = in.read(buffer)) > 0) {
-					zos.write(buffer, 0, len);
+				try (final FileInputStream in = new FileInputStream(file)) {
+					int len;
+					while ((len = in.read(buffer)) > 0) {
+						zos.write(buffer, 0, len);
+					}
 				}
-
-				in.close();
 			}
 			zos.closeEntry();
 
@@ -64,63 +61,57 @@ public class ZipUtil {
 	}
 
 	public String zipIt(final String fileNameWithoutExtension, final List<String> filenameList, final Project project, final ToolName tool)
-			throws IOException {
-		byte[] buffer = new byte[1024];
+		throws IOException {
+		final byte[] buffer = new byte[1024];
 
 		final String fileNameUnderWorkspaceDirectory = this.installationDirectoryUtil
-				.getTempFileInOutputDirectoryForProjectAndTool(fileNameWithoutExtension, ZIP_EXTENSION, project, tool);
-		FileOutputStream fos = new FileOutputStream(fileNameUnderWorkspaceDirectory);
-		ZipOutputStream zos = new ZipOutputStream(fos);
+			.getTempFileInOutputDirectoryForProjectAndTool(fileNameWithoutExtension, ZIP_EXTENSION, project, tool);
+		final FileOutputStream fos = new FileOutputStream(fileNameUnderWorkspaceDirectory);
+		try (final ZipOutputStream zos = new ZipOutputStream(fos)) {
 
-		for (String file : filenameList) {
+			for (final String file : filenameList) {
 
-			File f = new File(file);
-			ZipEntry ze = new ZipEntry(f.getName());
-			zos.putNextEntry(ze);
+				final File f = new File(file);
+				final ZipEntry ze = new ZipEntry(f.getName());
+				zos.putNextEntry(ze);
 
-			FileInputStream in = new FileInputStream(file);
+				try (final FileInputStream in = new FileInputStream(file)) {
 
-			int len;
-			while ((len = in.read(buffer)) > 0) {
-				zos.write(buffer, 0, len);
+					int len;
+					while ((len = in.read(buffer)) > 0) {
+						zos.write(buffer, 0, len);
+					}
+				}
 			}
 
-			in.close();
+			zos.closeEntry();
 		}
-
-		zos.closeEntry();
-		// remember close it
-		zos.close();
 
 		return fileNameUnderWorkspaceDirectory;
 	}
-	
-	public void extractZip(String zipFile, String destination) {
 
-		File file = new File(zipFile);
-		InputStream input;
+	public void extractZip(final String zipFile, final String destination) {
+
+		final File file = new File(zipFile);
+		final InputStream input;
 
 		try {
 
 			input = new FileInputStream(file);
-			ZipInputStream zip = new ZipInputStream(input);
-			ZipEntry entry = zip.getNextEntry();
+			try (final ZipInputStream zip = new ZipInputStream(input)) {
+				ZipEntry entry = zip.getNextEntry();
 
-			while (entry != null) {
+				while (entry != null) {
 
-				String filePath = destination + File.separator + entry.getName();
+					final String filePath = destination + File.separator + entry.getName();
 
-				extractFile(zip, filePath);
+					extractFile(zip, filePath);
 
-				entry = zip.getNextEntry();
+					entry = zip.getNextEntry();
+				}
 			}
-
-			zip.close();
 			input.close();
-
-		} catch (FileNotFoundException e) {
-			ZipUtil.LOG.error(e.getMessage(), e);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			ZipUtil.LOG.error(e.getMessage(), e);
 		}
 
@@ -129,38 +120,34 @@ public class ZipUtil {
 	public File extractZipSpecificFile(final String zipFile, final String fileNameToExtract, final Project project, final ToolName tool) {
 
 		File extractedFile = null;
-		File file = new File(zipFile);
-		InputStream input;
+		final File file = new File(zipFile);
+		final InputStream input;
 
 		try {
 
 			input = new FileInputStream(file);
-			ZipInputStream zip = new ZipInputStream(input);
-			ZipEntry entry = zip.getNextEntry();
+			try (final ZipInputStream zip = new ZipInputStream(input)) {
+				ZipEntry entry = zip.getNextEntry();
 
-			while (entry != null) {
+				while (entry != null) {
 
-				if (entry.getName().toLowerCase().contains(fileNameToExtract.toLowerCase())) {
-					final String fileNameUnderWorkspaceDirectory =
+					if (entry.getName().toLowerCase().contains(fileNameToExtract.toLowerCase())) {
+						final String fileNameUnderWorkspaceDirectory =
 							this.installationDirectoryUtil.getInputDirectoryForProjectAndTool(project, tool);
-					String filePath = fileNameUnderWorkspaceDirectory + File.separator + entry.getName();
-					this.extractFile(zip, filePath);
-					extractedFile = new File(filePath);
-					break;
+						final String filePath = fileNameUnderWorkspaceDirectory + File.separator + entry.getName();
+						this.extractFile(zip, filePath);
+						extractedFile = new File(filePath);
+						break;
+					}
+					entry = zip.getNextEntry();
 				}
-				entry = zip.getNextEntry();
 			}
-
-			zip.close();
 			input.close();
 
 			if (extractedFile != null) {
 				return extractedFile;
 			}
-
-		} catch (FileNotFoundException e) {
-			ZipUtil.LOG.error(e.getMessage(), e);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			ZipUtil.LOG.error(e.getMessage(), e);
 		}
 
@@ -168,13 +155,13 @@ public class ZipUtil {
 
 	}
 
-	private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
-		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
-		byte[] bytesIn = new byte[ZipUtil.BUFFER_SIZE];
-		int read = 0;
-		while ((read = zipIn.read(bytesIn)) != -1) {
-			bos.write(bytesIn, 0, read);
+	private void extractFile(final ZipInputStream zipIn, final String filePath) throws IOException {
+		try (final BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath))) {
+			final byte[] bytesIn = new byte[ZipUtil.BUFFER_SIZE];
+			int read = 0;
+			while ((read = zipIn.read(bytesIn)) != -1) {
+				bos.write(bytesIn, 0, read);
+			}
 		}
-		bos.close();
 	}
 }
