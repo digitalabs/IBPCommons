b2fcc82d675b7388a65353849a11ace4c4ab6d51
Darla Hermo <darla@leafnode.io>
2019-05-29 16:00:17 +0800

    Fix failing test and removed invalid tests
    IBP-2256

--- src/test/java/org/generationcp/commons/help/document/HelpButtonClickListenerTest.java
+++ src/test/java/org/generationcp/commons/help/document/HelpButtonClickListenerTest.java
@@ -1,24 +1,22 @@
 
 package org.generationcp.commons.help.document;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Properties;
-
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import com.vaadin.terminal.ExternalResource;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Window;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.exceptions.verification.TooLittleActualInvocations;
 
-import com.vaadin.terminal.ExternalResource;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.Window;
+import java.net.MalformedURLException;
+import java.util.Properties;
 
 public class HelpButtonClickListenerTest {
 
@@ -26,7 +24,7 @@ public class HelpButtonClickListenerTest {
 
 	private HelpButtonClickListener listener;
 	private HelpModule link;
-	
+
 	@Mock
 	private Properties helpProperties;
 
@@ -46,7 +44,7 @@ public class HelpButtonClickListenerTest {
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		this.link = HelpModule.MANAGE_LIST;
-		this.listener = Mockito.spy(new HelpButtonClickListener(this.link));
+		this.listener = new HelpButtonClickListener(this.link);
 		this.listener.setHelpProperties(this.helpProperties);
 
 		Mockito.when(this.event.getComponent()).thenReturn(this.component);
@@ -58,63 +56,18 @@ public class HelpButtonClickListenerTest {
 
 	@Test
 	public void testButtonClick_ForOnlineLink() throws MalformedURLException {
-		String onlineLink = "www.google.com";
+		final String onlineLink = "www.google.com";
 		Mockito.doReturn(onlineLink).when(this.helpProperties).getProperty(this.link.getPropertyName());
-		Mockito.when(this.listener.getOnlineLink(this.link)).thenReturn(onlineLink);
 
-		URL onlineURL = new URL(HelpDocumentUtil.getOnLineLink(onlineLink));
-		ExternalResource tutorialLink = new ExternalResource(onlineURL);
-
+		final ArgumentCaptor<ExternalResource> resourceCaptor = ArgumentCaptor.forClass(ExternalResource.class);
 		this.listener.buttonClick(this.event);
 
 		try {
-			Mockito.verify(this.window, Mockito.times(1)).open(tutorialLink, " _BLANK");
-		} catch (TooLittleActualInvocations e) {
+			Mockito.verify(this.window, Mockito.times(1)).open(resourceCaptor.capture(), ArgumentMatchers.eq(" _BLANK"));
+			Assert.assertEquals("https://" + onlineLink, resourceCaptor.getValue().getURL());
+		} catch (final TooLittleActualInvocations e) {
 			Assert.fail("Expecting to call the method that opens an online link.");
 		}
-	}
-
-	@Test
-	public void testButtonClick_ForOfflineLinkWithEmptyLink() throws MalformedURLException {
-		String offlineLink = "www.offline.com";
-		URL offlineURL = new URL(HelpDocumentUtil.getOnLineLink(offlineLink));
-
-		Mockito.doReturn(offlineLink).when(this.helpProperties).getProperty(this.link.getPropertyName());
-
-		this.listener.buttonClick(this.event);
-
-		try {
-			Mockito.verify(this.parentComponent, Mockito.times(1)).getWindow();
-		} catch (TooLittleActualInvocations e) {
-			Assert.fail("Expecting to call the method that opens an help pop up but didn't.");
-		}
-	}
-
-	@Test
-	public void testButtonClick_ForOfflineLinkWithNonEmptyLink() throws MalformedURLException, MiddlewareQueryException {
-		String offlineLink = "www.offline.com";
-		URL offlineURL = new URL(HelpDocumentUtil.getOnLineLink(offlineLink));
-
-		Mockito.doReturn(offlineLink).when(this.helpProperties).getProperty(this.link.getPropertyName());
-
-		ExternalResource tutorialLink = new ExternalResource(offlineURL);
-
-		this.listener.buttonClick(this.event);
-
-		if (new File(INSTALLATION_PATH.concat("\\Documents")).exists()) {
-			try {
-				Mockito.verify(this.window, Mockito.times(1)).open(tutorialLink, " _BLANK");
-			} catch (TooLittleActualInvocations e) {
-				Assert.fail("Expecting to call the method that opens an online link.");
-			}
-		} else {
-			try {
-				Mockito.verify(this.parentComponent, Mockito.times(1)).getWindow();
-			} catch (TooLittleActualInvocations e) {
-				Assert.fail("Expecting to call the method that opens an help pop up but didn't.");
-			}
-		}
-
 	}
 
 }
