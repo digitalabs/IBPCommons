c8cf9ed393a022bd291ff4bb8d8d55947ffa18bd
Darla Hermo <darla@leafnode.io>
2019-07-05 15:49:03 +0800

    Revert the full pervious logic of [NUMBER] process code
    IBP-2839

--- src/main/java/org/generationcp/commons/ruleengine/naming/expression/NumberExpression.java
+++ src/main/java/org/generationcp/commons/ruleengine/naming/expression/NumberExpression.java
@@ -1,6 +1,7 @@
 
 package org.generationcp.commons.ruleengine.naming.expression;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -18,13 +19,48 @@ public class NumberExpression extends BaseExpression implements Expression {
 
 	@Override
 	public void apply(final List<StringBuilder> values, final AdvancingSource source, final String capturedText) {
-		for (final StringBuilder container : values) {
-			if (source.getPlantsSelected() != null && source.getPlantsSelected() > 1) {
-				final Integer newValue = source.getPlantsSelected();
-				this.replaceExpressionWithValue(container, newValue != null ? newValue.toString() : "");
-			} else {
-				this.replaceExpressionWithValue(container, "");
+		if (source.isForceUniqueNameGeneration()) {
+			for (StringBuilder container : values) {
+				this.replaceExpressionWithValue(container, "(" + (source.getCurrentMaxSequence() + 1) + ")");
+
 			}
+
+			return;
+		}
+
+		if (source.isBulk()) {
+			for (StringBuilder container : values) {
+				if (source.getPlantsSelected() != null && source.getPlantsSelected() > 1) {
+					Integer newValue = source.getPlantsSelected();
+					this.replaceExpressionWithValue(container, newValue != null ? newValue.toString() : "");
+				} else {
+					this.replaceExpressionWithValue(container, "");
+				}
+			}
+		} else {
+			List<StringBuilder> newNames = new ArrayList<StringBuilder>();
+			int startCount = 1;
+
+			if (source.getCurrentMaxSequence() > -1) {
+				startCount = source.getCurrentMaxSequence() + 1;
+			}
+
+			for (StringBuilder value : values) {
+				if (source.getPlantsSelected() != null && source.getPlantsSelected() > 0) {
+
+					for (int i = startCount; i < startCount + source.getPlantsSelected(); i++) {
+						StringBuilder newName = new StringBuilder(value);
+						this.replaceExpressionWithValue(newName, String.valueOf(i));
+						newNames.add(newName);
+					}
+				} else {
+					this.replaceExpressionWithValue(value, "");
+					newNames.add(value);
+				}
+			}
+
+			values.clear();
+			values.addAll(newNames);
 		}
 	}
 
