f4104200aec6829dc8dbe95e3ce5ae7841c0755f
Clarysabel Tovar <clarysabel@leafnode.io>
2019-07-30 16:11:34 -0300

    Removed not needed function IBP-2792

--- src/main/java/org/generationcp/commons/security/AuthorizationUtil.java
+++ src/main/java/org/generationcp/commons/security/AuthorizationUtil.java
@@ -2,12 +2,10 @@ package org.generationcp.commons.security;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
-import org.fest.util.Collections;
 import org.generationcp.middleware.pojos.workbench.Role;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.GrantedAuthority;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -16,18 +14,6 @@ public class AuthorizationUtil {
 	
 	protected static final String ACCESS_DENIED_MESSAGE = "Access Denied. User does not have appropriate role to access the functionality.";
 
-	public static void preAuthorize(final List<Role> configuredRoles) {
-		if (Collections.isNullOrEmpty(configuredRoles)) {
-			throw new AccessDeniedException("Access Denied. No role is configured to access this functionality.");
-		}
-		final List<String> permittedRoleNames = new ArrayList<>();
-		for (final Role role : configuredRoles) {
-			permittedRoleNames.add(role.getName());
-		}
-		AuthorizationUtil.preAuthorizeUser(permittedRoleNames);
-		
-	}
-	
 	private static void preAuthorizeUser(final List<String> roleNames){
 		final Collection<? extends GrantedAuthority> authorities = SecurityUtil.getLoggedInUserRoles();
 		final List<String> capitalizedRoles = Lists.transform(roleNames, new Function<String, String>() {
@@ -48,7 +34,7 @@ public class AuthorizationUtil {
 	}
 	
 	public static void preAuthorizeAdminAuthority() {
-		preAuthorizeUser(Arrays.asList(Role.SUPERADMIN, Role.ADMIN));
+		preAuthorizeUser(Arrays.asList(Role.SUPERADMIN));
 	}
 	
 	public static Boolean isSuperAdminUser() {
--- src/test/java/org/generationcp/commons/security/AuthorizationUtilTest.java
+++ src/test/java/org/generationcp/commons/security/AuthorizationUtilTest.java
@@ -1,7 +1,5 @@
 package org.generationcp.commons.security;
 
-import java.util.Collections;
-
 import org.generationcp.middleware.pojos.workbench.Role;
 import org.junit.Test;
 import org.springframework.security.access.AccessDeniedException;
@@ -17,47 +15,6 @@ public class AuthorizationUtilTest {
 	private static final String PASSWORD = "admin2";
 	private static final String USERNAME = "admin1";
 	private UsernamePasswordAuthenticationToken loggedInUser;
-
-	@Test
-	public void testPreAuthorizeSuccessWithAdminConfiguredAndAdminUserLoggedIn() throws Exception {
-		SimpleGrantedAuthority roleAuthority = new SimpleGrantedAuthority(SecurityUtil.ROLE_PREFIX + "ADMIN");
-
-		this.loggedInUser = new UsernamePasswordAuthenticationToken("admin", "admin", Lists.newArrayList(roleAuthority));
-		SecurityContextHolder.getContext().setAuthentication(this.loggedInUser);
-		try {
-			AuthorizationUtil.preAuthorize(Collections.singletonList(new Role(1, "Admin")));
-		} catch (AccessDeniedException e) {
-			Assert.fail("Should not throw Access Denied exception.");
-		}
-	}
-
-	@Test
-	public void testPreAuthorizeFailWithAdminConfiguredAndTechnicianUserLoggedIn() throws Exception {
-		SimpleGrantedAuthority roleAuthority = new SimpleGrantedAuthority(SecurityUtil.ROLE_PREFIX + "TECHNICIAN");
-
-		this.loggedInUser = new UsernamePasswordAuthenticationToken(USERNAME, PASSWORD, Lists.newArrayList(roleAuthority));
-		SecurityContextHolder.getContext().setAuthentication(this.loggedInUser);
-		try {
-			AuthorizationUtil.preAuthorize(Collections.singletonList(new Role(1, "Admin")));
-			Assert.fail("Expecting to throws AccessDeniedException but did not.");
-		} catch (AccessDeniedException e) {
-			Assert.assertEquals("Access Denied. User does not have appropriate role to access the functionality.", e.getMessage());
-		}
-	}
-
-	@Test
-	public void testPreAuthorizefailWithNoRoleConfiguredAndTechnicianUserLoggedIn() throws Exception {
-		SimpleGrantedAuthority roleAuthority = new SimpleGrantedAuthority(SecurityUtil.ROLE_PREFIX + "TECHNICIAN");
-
-		this.loggedInUser = new UsernamePasswordAuthenticationToken(USERNAME, PASSWORD, Lists.newArrayList(roleAuthority));
-		SecurityContextHolder.getContext().setAuthentication(this.loggedInUser);
-		try {
-			AuthorizationUtil.preAuthorize(null);
-			Assert.fail("Expecting to throws AccessDeniedException but did not.");
-		} catch (AccessDeniedException e) {
-			Assert.assertEquals("Access Denied. No role is configured to access this functionality.", e.getMessage());
-		}
-	}
 	
 	@Test
 	public void testPreAuthorizeAdminAuthorityWithAdminRoleLoggedIn() {
