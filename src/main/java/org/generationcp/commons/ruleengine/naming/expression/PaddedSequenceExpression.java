b9bf6992f6fd31ca9b616c59947f631e4b62f6d3
Darla Hermo <darla@leafnode.io>
2019-08-01 13:35:29 +0800

    Implement PADSEQ expression for naming
    IBP-1382

---
+++ src/main/java/org/generationcp/commons/ruleengine/naming/expression/PaddedSequenceExpression.java
@@ -0,0 +1,17 @@
+package org.generationcp.commons.ruleengine.naming.expression;
+
+import org.generationcp.commons.ruleengine.ExpressionUtils;
+
+public class PaddedSequenceExpression extends SequenceExpression {
+
+	@Override
+	public Integer getNumberOfDigits(final StringBuilder container) {
+		return ExpressionUtils.getNumberOfDigitsFromKey(this.getExpressionKey(), container);
+	}
+
+	@Override
+	public String getExpressionKey() {
+		return org.generationcp.commons.ruleengine.coding.expression.PaddedSequenceExpression.PATTERN_KEY;
+	}
+
+}
