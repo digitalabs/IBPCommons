2ceb6e6c0dfb2fdc640b4ca0865db02af86ebadd
Darla Hermo <darla@leafnode.io>
2019-08-02 10:43:42 +0800

    Add @Component annotation so that PADSEQ expression is registered
    IBP-1382

--- src/main/java/org/generationcp/commons/ruleengine/naming/expression/PaddedSequenceExpression.java
+++ src/main/java/org/generationcp/commons/ruleengine/naming/expression/PaddedSequenceExpression.java
@@ -1,7 +1,9 @@
 package org.generationcp.commons.ruleengine.naming.expression;
 
 import org.generationcp.commons.ruleengine.ExpressionUtils;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PaddedSequenceExpression extends SequenceExpression {
 
 	@Override
