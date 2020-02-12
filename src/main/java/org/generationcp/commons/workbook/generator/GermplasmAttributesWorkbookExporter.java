0306cdd8e3919386024b7a85c6c3cdaba91d927e
Gelli Samson <gelli@leafnode.io>
2020-02-12 17:34:50 +0800

    Apply suggested code optimization. IBP-3448-AddedAttributeSort

--- src/main/java/org/generationcp/commons/workbook/generator/GermplasmAttributesWorkbookExporter.java
+++ src/main/java/org/generationcp/commons/workbook/generator/GermplasmAttributesWorkbookExporter.java
@@ -2,6 +2,7 @@ package org.generationcp.commons.workbook.generator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.Resource;
 
@@ -9,6 +10,7 @@ import org.apache.poi.ss.usermodel.CellStyle;
 import org.generationcp.commons.parsing.ExcelCellStyleBuilder;
 import org.generationcp.middleware.manager.api.GermplasmDataManager;
 import org.generationcp.middleware.pojos.UserDefinedField;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,22 +18,26 @@ public class GermplasmAttributesWorkbookExporter extends GermplasmAddedColumnExp
 
 	@Resource
 	private GermplasmDataManager germplasmManager;
-	
+
 	private List<String> addedAttributeColumns = new ArrayList<>();
-	
+
 	@Override
 	List<UserDefinedField> getSourceItems() {
 		final List<UserDefinedField> attributeTypeColumns = new ArrayList<>();
 		//columnsInfo is null when exporting germplasm list from Study Manager
-		if(this.columnsInfo != null) {
+		if (this.columnsInfo != null) {
 			final List<UserDefinedField> attributeTypes = this.germplasmManager.getAllAttributesTypes();
 			final List<String> addedColumns = this.columnsInfo.getAddedColumnCurrentSort();
-			for(final String addedCol : addedColumns) {
-				final UserDefinedField field;
-				field = attributeTypes.stream().filter(userDefinedField -> userDefinedField.getFcode().toUpperCase().equalsIgnoreCase(addedCol)).findFirst().get();
-				if(field!=null){
-					this.addedAttributeColumns.add(field.getFcode().toUpperCase());
-					attributeTypeColumns.add(field);
+			for (final String addedCol : addedColumns) {
+				final Optional<UserDefinedField> optional =
+					attributeTypes.stream().filter(userDefinedField -> userDefinedField.getFcode().toUpperCase().equalsIgnoreCase(addedCol))
+						.findFirst();
+				if (optional.isPresent()) {
+					final UserDefinedField field = optional.get();
+					if (field != null) {
+						this.addedAttributeColumns.add(field.getFcode().toUpperCase());
+						attributeTypeColumns.add(field);
+					}
 				}
 			}
 
@@ -97,5 +103,5 @@ public class GermplasmAttributesWorkbookExporter extends GermplasmAddedColumnExp
 	public void setAddedAttributeColumns(final List<String> addedAttributeColumns) {
 		this.addedAttributeColumns = addedAttributeColumns;
 	}
-	
+
 }
