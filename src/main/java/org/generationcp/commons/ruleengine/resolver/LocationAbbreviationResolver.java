25566ffb333dbc39fd1f38ac1e3deb20a1997eb9
Clarysabel Tovar <clarysabel@leafnode.io>
2020-12-17 16:03:42 -0300

    Cleaning code IBP-3882

--- src/main/java/org/generationcp/commons/ruleengine/resolver/LocationAbbreviationResolver.java
+++ src/main/java/org/generationcp/commons/ruleengine/resolver/LocationAbbreviationResolver.java
@@ -1,8 +1,5 @@
 package org.generationcp.commons.ruleengine.resolver;
 
-import com.google.common.base.Function;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.oms.TermId;
@@ -11,9 +8,12 @@ import org.generationcp.middleware.service.api.dataset.ObservationUnitRow;
 import org.generationcp.middleware.service.impl.study.StudyInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Revolves Location value for Nurseries and Trials.
@@ -36,23 +36,22 @@ public class LocationAbbreviationResolver implements KeyComponentValueResolver {
 	public String resolve() {
 		String location = "";
 
-		if (this.observationUnitRow != null) {
-			final ImmutableMap<Integer, ObservationUnitData> dataListMap =
-				Maps.uniqueIndex(this.observationUnitRow.getVariables().values(), new Function<ObservationUnitData, Integer>() {
+		if (!CollectionUtils.isEmpty(studyInstanceMap)) {
+			if (this.observationUnitRow != null) {
 
-					@Override
-					public Integer apply(final ObservationUnitData observationUnitData) {
-						return observationUnitData.getVariableId();
+				final Optional<ObservationUnitData> instanceNoUnitData =
+					Optional.of(observationUnitRow.getVariables().entrySet().stream().collect(Collectors
+						.toMap(k -> k.getValue().getVariableId(),
+							k -> k.getValue())).get(TermId.TRIAL_INSTANCE_FACTOR.getId()));
+
+				if (instanceNoUnitData.isPresent()) {
+					final String instanceNo = instanceNoUnitData.get().getValue();
+					if (instanceNo != null && studyInstanceMap.containsKey(Integer.valueOf(instanceNo))) {
+						location = studyInstanceMap.get(Integer.valueOf(instanceNo)).getLocationAbbreviation();
 					}
-				});
 
-			final String instanceNo = dataListMap.get(TermId.TRIAL_INSTANCE_FACTOR.getId()).getValue();
-			if (instanceNo != null && studyInstanceMap != null && studyInstanceMap.containsKey(Integer.valueOf(instanceNo))) {
-				location = studyInstanceMap.get(Integer.valueOf(instanceNo)).getLocationAbbreviation();
-			}
-
-		} else {
-			if (studyInstanceMap != null && !studyInstanceMap.isEmpty()) {
+				}
+			} else {
 				final StudyInstance firstInstance = studyInstanceMap.get(1);
 				if (firstInstance != null) {
 					location = firstInstance.getLocationAbbreviation();
@@ -62,11 +61,9 @@ public class LocationAbbreviationResolver implements KeyComponentValueResolver {
 
 		if (StringUtils.isBlank(location)) {
 			LocationAbbreviationResolver.LOG.debug(
-				"No LOCATION_ABBR(8189), LOCATION_NAME(8180) or TRIAL_INSTANCE(8170) variable was found in the study. "
-					+ "Or it is present but no value is set. Resolving location value to be an empty string.");
+				"No Location Abbreviation was resolved");
 			return "";
 		}
-
 		return location;
 	}
 
