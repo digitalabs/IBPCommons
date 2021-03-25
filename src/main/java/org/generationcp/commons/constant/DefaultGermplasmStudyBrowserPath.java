3e54455ba5408f48419c4619aa9bc75e8eeac907
Aldrin Batac <aldrin.batac@leafnode.io>
2021-03-25 16:39:28 +0800

    Remove unused code.
    IBP-4052 IBP-4414

--- src/main/java/org/generationcp/commons/constant/DefaultGermplasmStudyBrowserPath.java
+++ src/main/java/org/generationcp/commons/constant/DefaultGermplasmStudyBrowserPath.java
@@ -7,7 +7,6 @@ package org.generationcp.commons.constant;
 public interface DefaultGermplasmStudyBrowserPath {
 
 	String LIST_BROWSER_LINK = "ibpworkbench/maingpsb/germplasmlist-";
-	String GERMPLASM_BROWSER_LINK = "ibpworkbench/maingpsb/germplasm-";
 	String STUDY_BROWSER_LINK = "ibpworkbench/maingpsb/study-";
 	String GRAPHICAL_FILTERING_TOOL_LINK = "/ibpworkbench/controller/graphical-filtering?studyDbId=%s&crop=%s&restartApplication";
 	String GERMPLASM_DETAILS_LINK = "/ibpworkbench/main/app/#/germplasm-details/";
