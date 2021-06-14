5dd9fe7e4a7411e181902d60b14d332a0cf69f1d
Clarysabel Tovar <clarysabel@leafnode.io>
2021-06-14 15:34:24 -0300

    Fill With Attributes Fix IBP-4723

--- src/main/java/org/generationcp/commons/workbook/generator/GermplasmAttributesWorkbookExporter.java
+++ src/main/java/org/generationcp/commons/workbook/generator/GermplasmAttributesWorkbookExporter.java
@@ -2,8 +2,11 @@ package org.generationcp.commons.workbook.generator;
 
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.generationcp.commons.parsing.ExcelCellStyleBuilder;
-import org.generationcp.middleware.manager.api.GermplasmDataManager;
-import org.generationcp.middleware.pojos.UserDefinedField;
+import org.generationcp.middleware.domain.ontology.Variable;
+import org.generationcp.middleware.domain.ontology.VariableType;
+import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
+import org.generationcp.middleware.manager.ontology.daoElements.VariableFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -15,21 +18,28 @@ import java.util.stream.Collectors;
 @Component
 public class GermplasmAttributesWorkbookExporter extends GermplasmAddedColumnExporter<Variable> {
 
+	@Autowired
+	private OntologyVariableDataManager ontologyVariableDataManager;
+
 	private List<String> addedAttributeColumns = new ArrayList<>();
 
 	@Override
-	List<UserDefinedField> getSourceItems() {
-		final List<UserDefinedField> attributeTypeColumns = new ArrayList<>();
+	List<Variable> getSourceItems() {
+		final List<Variable> attributeTypeColumns = new ArrayList<>();
 		//columnsInfo is null when exporting germplasm list from Study Manager
 		if (this.columnsInfo != null) {
-			final List<UserDefinedField> attributeTypes = this.germplasmManager.getAllAttributesTypes();
-			final Map<String, UserDefinedField>
-				attributeTypesMap = attributeTypes.stream().collect(Collectors.toMap(u -> u.getFcode().toUpperCase(), u -> u, (u1, u2) -> u1));
+			final VariableFilter variableFilter = new VariableFilter();
+			variableFilter.addVariableType(VariableType.GERMPLASM_PASSPORT);
+			variableFilter.addVariableType(VariableType.GERMPLASM_ATTRIBUTE);
+			final List<Variable> attributeTypes = this.ontologyVariableDataManager.getWithFilter(variableFilter);
+			final Map<String, Variable>
+				attributeTypesMap =
+				attributeTypes.stream().collect(Collectors.toMap(u -> u.getName().toUpperCase(), Function.identity(), (u1, u2) -> u1));
 
 			for (final String addedCol : this.columnsInfo.getColumns()) {
-				final UserDefinedField field = attributeTypesMap.get(addedCol.toUpperCase());
+				final Variable field = attributeTypesMap.get(addedCol);
 				if (field != null) {
-					this.addedAttributeColumns.add(field.getFcode().toUpperCase());
+					this.addedAttributeColumns.add(field.getName().toUpperCase());
 					attributeTypeColumns.add(field);
 				}
 			}
@@ -49,43 +59,43 @@ public class GermplasmAttributesWorkbookExporter extends GermplasmAddedColumnExp
 	}
 
 	@Override
-	String getName(final UserDefinedField source) {
-		return source.getFcode().toUpperCase();
+	String getName(final Variable source) {
+		return source.getName().toUpperCase();
 	}
 
 	@Override
-	String getDescription(final UserDefinedField source) {
-		return "Additional details about germplasm";
+	String getDescription(final Variable source) {
+		return source.getDefinition();
 	}
 
 	@Override
-	String getProperty(final UserDefinedField source) {
-		return "ATTRIBUTE";
+	String getProperty(final Variable source) {
+		return source.getProperty().getName().toUpperCase();
 	}
 
 	@Override
-	String getScale(final UserDefinedField source) {
-		return "TEXT";
+	String getScale(final Variable source) {
+		return source.getScale().getName().toUpperCase();
 	}
 
 	@Override
-	String getMethod(final UserDefinedField source) {
-		return "OBSERVED";
+	String getMethod(final Variable source) {
+		return source.getMethod().getName().toUpperCase();
 	}
 
 	@Override
-	String getValue(final UserDefinedField source) {
+	String getValue(final Variable source) {
 		return "";
 	}
 
 	@Override
-	String getDatatype(final UserDefinedField source) {
-		return "C";
+	String getDatatype(final Variable source) {
+		return source.getScale().getDataType().getDataTypeCode();
 	}
 
 	@Override
-	String getComments(final UserDefinedField source) {
-		return "Optional";
+	String getComments(final Variable source) {
+		return "";
 	}
 
 	@Override
