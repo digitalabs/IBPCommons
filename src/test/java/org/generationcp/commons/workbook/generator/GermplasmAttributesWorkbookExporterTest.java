6b5f51ad007d7cc89ac314e7ed25356fcd83fc0c
Clarysabel Tovar <clarysabel@leafnode.io>
2021-06-30 13:00:47 -0300

    Fixed tests IBP-4440

--- src/test/java/org/generationcp/commons/workbook/generator/GermplasmAttributesWorkbookExporterTest.java
+++ src/test/java/org/generationcp/commons/workbook/generator/GermplasmAttributesWorkbookExporterTest.java
@@ -6,6 +6,7 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.generationcp.commons.parsing.ExcelCellStyleBuilder;
 import org.generationcp.commons.parsing.GermplasmExportTestHelper;
+import org.generationcp.middleware.ContextHolder;
 import org.generationcp.middleware.domain.gms.GermplasmListNewColumnsInfo;
 import org.generationcp.middleware.domain.ontology.DataType;
 import org.generationcp.middleware.domain.ontology.Method;
@@ -14,12 +15,14 @@ import org.generationcp.middleware.domain.ontology.Scale;
 import org.generationcp.middleware.domain.ontology.Variable;
 import org.generationcp.middleware.interfaces.GermplasmExportSource;
 import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
+import org.generationcp.middleware.util.VariableValueUtil;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -56,26 +59,34 @@ public class GermplasmAttributesWorkbookExporterTest {
 
 	@Test
 	public void testGetSourceItems() {
-		final List<Variable> attributes = this.attributesWorkbookExporter.getSourceItems();
-		Assert.assertFalse(attributes.isEmpty());
-		final Variable attribute = attributes.get(0);
-		Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTES, attribute.getDefinition());
-		Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTE, attribute.getName());
+		try (final MockedStatic<ContextHolder> contextHolderMockedStatic = Mockito.mockStatic(ContextHolder.class)) {
+			contextHolderMockedStatic.when(() -> ContextHolder.getCurrentProgram())
+				.thenReturn(null);
+			final List<Variable> attributes = this.attributesWorkbookExporter.getSourceItems();
+			Assert.assertFalse(attributes.isEmpty());
+			final Variable attribute = attributes.get(0);
+			Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTES, attribute.getDefinition());
+			Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTE, attribute.getName());
+		}
 	}
 
 	@Test
 	public void testGenerateAddedColumnHeader() {
-		final Variable variable1 =
-			this.getVariable(1, GermplasmAttributesWorkbookExporterTest.NOTES, GermplasmAttributesWorkbookExporterTest.NOTES);
+		try (final MockedStatic<ContextHolder> contextHolderMockedStatic = Mockito.mockStatic(ContextHolder.class)) {
+			contextHolderMockedStatic.when(() -> ContextHolder.getCurrentProgram())
+				.thenReturn(null);
+			final Variable variable1 =
+				this.getVariable(1, GermplasmAttributesWorkbookExporterTest.NOTES, GermplasmAttributesWorkbookExporterTest.NOTES);
 
-		Mockito.when(this.ontologyVariableDataManager.getWithFilter(Mockito.any())).thenReturn(Collections.singletonList(variable1));
-		this.attributesWorkbookExporter.setColumnsInfo(GermplasmExportTestHelper.generateAddedColumnsInfo());
-		final HSSFSheet observationSheet = this.wb.createSheet("Observation");
-		final HSSFRow headerRow = observationSheet.createRow(0);
-		this.attributesWorkbookExporter.generateAddedColumnHeader(headerRow, 0);
-		Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTES, headerRow.getCell(0).getStringCellValue());
-		Assert.assertEquals(this.styleBuilder.getCellStyle(ExcelCellStyleBuilder.ExcelCellStyle.HEADING_STYLE_FACTOR),
-			headerRow.getCell(0).getCellStyle());
+			Mockito.when(this.ontologyVariableDataManager.getWithFilter(Mockito.any())).thenReturn(Collections.singletonList(variable1));
+			this.attributesWorkbookExporter.setColumnsInfo(GermplasmExportTestHelper.generateAddedColumnsInfo());
+			final HSSFSheet observationSheet = this.wb.createSheet("Observation");
+			final HSSFRow headerRow = observationSheet.createRow(0);
+			this.attributesWorkbookExporter.generateAddedColumnHeader(headerRow, 0);
+			Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTES, headerRow.getCell(0).getStringCellValue());
+			Assert.assertEquals(this.styleBuilder.getCellStyle(ExcelCellStyleBuilder.ExcelCellStyle.HEADING_STYLE_FACTOR),
+				headerRow.getCell(0).getCellStyle());
+		}
 	}
 
 	@Test
@@ -91,34 +102,42 @@ public class GermplasmAttributesWorkbookExporterTest {
 
 	@Test
 	public void testAddRowsToDescriptionSheet() {
-		final HSSFSheet descriptionSheet = this.wb.createSheet("Description");
-		this.attributesWorkbookExporter.addRowsToDescriptionSheet(descriptionSheet, 0, this.styleBuilder, this.columnsInfo);
-		final HSSFRow attributeRow = descriptionSheet.getRow(1);
+		try (final MockedStatic<ContextHolder> contextHolderMockedStatic = Mockito.mockStatic(ContextHolder.class)) {
+			contextHolderMockedStatic.when(() -> ContextHolder.getCurrentProgram())
+				.thenReturn(null);
+			final HSSFSheet descriptionSheet = this.wb.createSheet("Description");
+			this.attributesWorkbookExporter.addRowsToDescriptionSheet(descriptionSheet, 0, this.styleBuilder, this.columnsInfo);
+			final HSSFRow attributeRow = descriptionSheet.getRow(1);
 
-		final Variable attribute = this.attributesWorkbookExporter.getSourceItems().get(0);
-		Assert.assertEquals(this.attributesWorkbookExporter.getName(attribute), attributeRow.getCell(0).getStringCellValue());
-		Assert.assertEquals(this.attributesWorkbookExporter.getDescription(attribute), attributeRow.getCell(1).getStringCellValue());
-		Assert.assertEquals(this.attributesWorkbookExporter.getProperty(attribute), attributeRow.getCell(2).getStringCellValue());
-		Assert.assertEquals(this.attributesWorkbookExporter.getScale(attribute), attributeRow.getCell(3).getStringCellValue());
-		Assert.assertEquals(this.attributesWorkbookExporter.getMethod(attribute), attributeRow.getCell(4).getStringCellValue());
-		Assert.assertEquals(this.attributesWorkbookExporter.getDatatype(attribute), attributeRow.getCell(5).getStringCellValue());
-		Assert.assertEquals(this.attributesWorkbookExporter.getValue(attribute), attributeRow.getCell(6).getStringCellValue());
-		Assert.assertEquals(this.attributesWorkbookExporter.getComments(attribute), attributeRow.getCell(7).getStringCellValue());
+			final Variable attribute = this.attributesWorkbookExporter.getSourceItems().get(0);
+			Assert.assertEquals(this.attributesWorkbookExporter.getName(attribute), attributeRow.getCell(0).getStringCellValue());
+			Assert.assertEquals(this.attributesWorkbookExporter.getDescription(attribute), attributeRow.getCell(1).getStringCellValue());
+			Assert.assertEquals(this.attributesWorkbookExporter.getProperty(attribute), attributeRow.getCell(2).getStringCellValue());
+			Assert.assertEquals(this.attributesWorkbookExporter.getScale(attribute), attributeRow.getCell(3).getStringCellValue());
+			Assert.assertEquals(this.attributesWorkbookExporter.getMethod(attribute), attributeRow.getCell(4).getStringCellValue());
+			Assert.assertEquals(this.attributesWorkbookExporter.getDatatype(attribute), attributeRow.getCell(5).getStringCellValue());
+			Assert.assertEquals(this.attributesWorkbookExporter.getValue(attribute), attributeRow.getCell(6).getStringCellValue());
+			Assert.assertEquals(this.attributesWorkbookExporter.getComments(attribute), attributeRow.getCell(7).getStringCellValue());
+		}
 	}
 
 	@Test
 	public void testGetSourceItemsDuplicate() {
-		final Variable variable1 =
-			this.getVariable(1, GermplasmAttributesWorkbookExporterTest.NOTE, GermplasmAttributesWorkbookExporterTest.NOTES);
-		final Variable variable2 = this.getVariable(1, GermplasmAttributesWorkbookExporterTest.NOTE, "-");
-		Mockito.when(this.ontologyVariableDataManager.getWithFilter(Mockito.any())).thenReturn(Arrays.asList(variable1, variable2));
-		Mockito.when(this.columnsInfo.getColumns()).thenReturn(Collections.singletonList(GermplasmAttributesWorkbookExporterTest.NOTE));
-		final List<Variable> attributes = this.attributesWorkbookExporter.getSourceItems();
-		Assert.assertFalse(attributes.isEmpty());
-		Assert.assertEquals("Returned value is 1 ", 1, attributes.size());
-		final Variable attribute = attributes.get(0);
-		Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTES, attribute.getDefinition());
-		Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTE, attribute.getName());
+		try (final MockedStatic<ContextHolder> contextHolderMockedStatic = Mockito.mockStatic(ContextHolder.class)) {
+			contextHolderMockedStatic.when(() -> ContextHolder.getCurrentProgram())
+				.thenReturn(null);
+			final Variable variable1 =
+				this.getVariable(1, GermplasmAttributesWorkbookExporterTest.NOTE, GermplasmAttributesWorkbookExporterTest.NOTES);
+			final Variable variable2 = this.getVariable(1, GermplasmAttributesWorkbookExporterTest.NOTE, "-");
+			Mockito.when(this.ontologyVariableDataManager.getWithFilter(Mockito.any())).thenReturn(Arrays.asList(variable1, variable2));
+			Mockito.when(this.columnsInfo.getColumns()).thenReturn(Collections.singletonList(GermplasmAttributesWorkbookExporterTest.NOTE));
+			final List<Variable> attributes = this.attributesWorkbookExporter.getSourceItems();
+			Assert.assertFalse(attributes.isEmpty());
+			Assert.assertEquals("Returned value is 1 ", 1, attributes.size());
+			final Variable attribute = attributes.get(0);
+			Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTES, attribute.getDefinition());
+			Assert.assertEquals(GermplasmAttributesWorkbookExporterTest.NOTE, attribute.getName());
+		}
 	}
 
 	private List<Variable> getAttributeVariables() {
